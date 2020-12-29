5c728d630da985c2d7cfc108cab8e7cfbb511d05
Joey Santoro <joseph.santoro@duke.edu>
2020-12-29 14:44:21 -0800

    cleanup and natspec OracleRef

--- contracts/refs/IOracleRef.sol
+++ contracts/refs/IOracleRef.sol
@@ -1,10 +1,39 @@
 pragma solidity ^0.6.0;
 pragma experimental ABIEncoderV2;
 
+import "../oracle/IOracle.sol";
 import "../external/Decimal.sol";
 
+/// @title A oracle Reference contract
+/// @author Fei Protocol
+/// @notice defines some utilities around interacting with the referenced oracle
 interface IOracleRef {
+	// Governor only state mutating API
+
+	/// @notice sets the referenced oracle
+	/// @param _oracle the new oracle to reference
 	function setOracle(address _oracle) external;
+
+	// Open state mutating API
+
+	/// @notice updates the referenced oracle
+	/// @return true if the update is effective
 	function updateOracle() external returns(bool);
+
+	// Getters
+
+	/// @notice the oracle reference by the contract
+	/// @return the IOracle implementation address
+	function oracle() external view returns(IOracle);
+
+	/// @notice the peg price of the referenced oracle
+	/// @return the peg as a Decimal
+	/// @dev the peg is defined as FEI per X with X being ETH, dollars, etc
 	function peg() external view returns(Decimal.D256 memory);
+
+	/// @notice invert a peg price
+	/// @param price the peg price to invert
+	/// @return the inverted peg as a Decimal
+	/// @dev the inverted peg would be X per FEI
+	function invert(Decimal.D256 calldata price) external pure returns(Decimal.D256 memory);
 }
--- contracts/refs/OracleRef.sol
+++ contracts/refs/OracleRef.sol
@@ -1,37 +1,36 @@
 pragma solidity ^0.6.0;
 pragma experimental ABIEncoderV2;
 
-import "../oracle/IOracle.sol";
-import "../external/Decimal.sol";
-import "../refs/CoreRef.sol";
+import "./IOracleRef.sol";
+import "./CoreRef.sol";
 
-abstract contract OracleRef is CoreRef {
+/// @title Abstract implementation of IOracleRef
+/// @author Fei Protocol
+abstract contract OracleRef is CoreRef, IOracleRef {
 	using Decimal for Decimal.D256;
 
-	IOracle public oracle;
+	IOracle public override oracle;
 
     event OracleUpdate(address indexed _oracle);
 
-	constructor(address _core, address _oracle) public
-		CoreRef(_core) 
-    {
+	constructor(address _core, address _oracle) public CoreRef(_core) {
         _setOracle(_oracle);
     }
 
-	function setOracle(address _oracle) public onlyGovernor {
+	function setOracle(address _oracle) public override onlyGovernor {
 		_setOracle(_oracle);
         emit OracleUpdate(_oracle);
 	}
 
-    function invert(Decimal.D256 memory price) public pure returns(Decimal.D256 memory) {
+    function invert(Decimal.D256 memory price) public override pure returns(Decimal.D256 memory) {
     	return Decimal.one().div(price);
     }
 
-    function updateOracle() public returns(bool) {
+    function updateOracle() public override returns(bool) {
     	return oracle.update();
     }
 
-    function peg() public view returns(Decimal.D256 memory) {
+    function peg() public override view returns(Decimal.D256 memory) {
     	(Decimal.D256 memory _peg, bool valid) = oracle.read();
     	require(valid, "OracleRef: oracle invalid");
     	return _peg;
