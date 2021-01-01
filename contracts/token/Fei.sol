a0acb0edbd3c5a7e6fd9c4db8a5abf034daaf6f8
Joey Santoro <joseph.santoro@duke.edu>
2021-01-01 13:32:34 -0800

    cleanup fei

--- contracts/token/Fei.sol
+++ contracts/token/Fei.sol
@@ -1,41 +1,44 @@
 pragma solidity ^0.6.0;
 pragma experimental ABIEncoderV2;
 
-import "./IIncentive.sol";
-import "../refs/CoreRef.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./IIncentive.sol";
+import "./IFei.sol";
+import "../refs/CoreRef.sol";
 
 /// @title IFei implementation
 /// @author Fei Protocol
-contract Fei is ERC20, ERC20Burnable, CoreRef {
+contract Fei is IFei, ERC20, ERC20Burnable, CoreRef {
 
-    mapping (address => address) public incentiveContract;
+    mapping (address => address) public override incentiveContract;
 
-    event Minting(address indexed to, address indexed minter, uint amount);
-    event Burning(address indexed to, address indexed burner, uint amount);
-    event IncentiveContractUpdate(address indexed _incentivized, address indexed _incentiveContract);
+    /// @notice Fei token constructor
+    /// @param core Fei Core address to reference
+	constructor(address core) public ERC20("Fei USD", "FEI") CoreRef(core) {}
 
-	constructor(address core) public
-	   ERC20("Fei USD", "FEI") 
-       CoreRef(core) {}
-
-    function mint(address account, uint amount) public onlyMinter {
-        _mint(account, amount);
-        emit Minting(account, msg.sender, amount);
-    }
-
-    function burnFrom(address account, uint amount) public override onlyBurner {
-        _burn(account, amount);
-        emit Burning(account, msg.sender, amount);
-    }
-
-    function setIncentiveContract(address account, address incentive) public onlyGovernor {
+    function setIncentiveContract(address account, address incentive) external override onlyGovernor {
         incentiveContract[account] = incentive;
         emit IncentiveContractUpdate(account, incentive);
     }
 
+    function mint(address account, uint amount) external override onlyMinter {
+        _mint(account, amount);
+        emit Minting(account, msg.sender, amount);
+    }
+
+    function burn(uint amount) public override(IFei, ERC20Burnable) {
+        super.burn(amount);
+        emit Burning(msg.sender, msg.sender, amount);
+    }
+
+    function burnFrom(address account, uint amount) public override(IFei, ERC20Burnable) onlyBurner {
+        _burn(account, amount);
+        emit Burning(account, msg.sender, amount);
+    }
+
     function _beforeTokenTransfer(address from, address to, uint amount) internal override {
+        // If not minting or burning
         if (from != address(0) && to != address(0)) {
             _checkAndApplyIncentives(from, to, amount);      
         }
@@ -47,16 +50,19 @@ contract Fei is ERC20, ERC20Burnable, CoreRef {
         if (senderIncentive != address(0)) {
             IIncentive(senderIncentive).incentivize(sender, recipient, msg.sender, amount);
         }
+
         // incentive on recipient
         address recipientIncentive = incentiveContract[recipient];
         if (recipientIncentive != address(0)) {
             IIncentive(recipientIncentive).incentivize(sender, recipient, msg.sender, amount);
         }
+
         // incentive on operator
         address operatorIncentive = incentiveContract[msg.sender];
         if (msg.sender != sender && msg.sender != recipient && operatorIncentive != address(0)) {
             IIncentive(operatorIncentive).incentivize(sender, recipient, msg.sender, amount);
         }
+
         // all incentive
         address allIncentive = incentiveContract[address(0)];
         if (allIncentive != address(0)) {
--- contracts/token/IFei.sol
+++ contracts/token/IFei.sol
@@ -6,36 +6,41 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 /// @author Fei Protocol
 interface IFei is IERC20 {
 
-    // State changing api
+	// ----------- Events -----------
 
-    /// @notice burn FEI tokens from sender
+    event Minting(address indexed _to, address indexed _minter, uint _amount);
+
+    event Burning(address indexed _to, address indexed _burner, uint _amount);
+
+    event IncentiveContractUpdate(address indexed _incentivized, address indexed _incentiveContract);
+
+    // ----------- State changing api -----------
+
+    /// @notice burn FEI tokens from caller
     /// @param amount the amount to burn
     function burn(uint amount) external;
 
-    // Burner only state changing api
+    // ----------- Burner only state changing api -----------
 
     /// @notice burn FEI tokens from specified account
     /// @param account the account to burn from
     /// @param amount the amount to burn
     function burnFrom(address account, uint amount) external;
 
-    // Minter only state changing api
+    // ----------- Minter only state changing api -----------
 
     /// @notice mint FEI tokens
     /// @param account the account to mint to
     /// @param amount the amount to mint
     function mint(address account, uint amount) external;
 
-    // Governor only state changing api
+    // ----------- Governor only state changing api -----------
 
     /// @param account the account to incentivize
     /// @param incentive the associated incentive contract
     function setIncentiveContract(address account, address incentive) external;
 
-    // Getters
-
-    /// @notice number of decimals of ERC20
-    function decimals() external view returns(uint);
+    // ----------- Getters -----------
 
     /// @notice get associated incentive contract
     /// @param account the address to check
