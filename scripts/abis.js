1d95312eafb83dded66832012ba41168c3993887
Joey Santoro <joseph.santoro@duke.edu>
2021-01-15 11:49:49 -0800

    abi export tool

---
+++ scripts/abis.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const fs = require('fs');
+const dir = process.env.GRAPH_ABI_DIR;
+const abisDir = dir + '/abis/';
+const buildDir = './build/contracts/';
+if (!fs.existsSync(dir)) {
+	throw new Error('Graph dir not exists');
+}
+if (!fs.existsSync(abisDir)) {
+	fs.mkdirSync(abisDir);
+}
+if (!fs.existsSync(buildDir)) {
+	throw new Error('No truffle build/contracts directory');
+}
+const files = fs.readdirSync(buildDir);
+for (var i = files.length - 1; i >= 0; i--) {
+	var name = files[i];
+	var contract = JSON.parse(fs.readFileSync(buildDir + name), 'utf8');
+	var abi = JSON.stringify(contract.abi);
+	fs.writeFileSync(abisDir + name, abi, 'utf8');
+	console.log(name + ': Done');
+}
