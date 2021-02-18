4e2303b6e9b02a23a46a0aa57c534f4482c39d32
Joey Santoro <joseph.santoro@duke.edu>
2021-02-18 13:18:43 -0800

    integration testing setup

--- contracts/orchestration/CoreOrchestrator.sol
+++ contracts/orchestration/CoreOrchestrator.sol
@@ -18,13 +18,18 @@ interface ITribe {
 contract CoreOrchestrator is Ownable {
     address public admin;
 
+    bool public constant ROPSTEN = false;
+
     // ----------- Uniswap Addresses -----------
-    address public constant ETH_USDC_UNI_PAIR =
+    address public constant ETH_USDC_UNI_PAIR = ROPSTEN ? 
+        address(0x681A4164703351d6AceBA9D7038b573b444d3353) :
         address(0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc);
+
     address public constant ROUTER =
         address(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
 
-    address public constant WETH =
+    address public constant WETH = ROPSTEN ?
+        address(0xc778417E063141139Fce010982780140Aa0cD5Ab) :
         address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
     IUniswapV2Factory public constant UNISWAP_FACTORY =
         IUniswapV2Factory(0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f);
@@ -33,30 +38,30 @@ contract CoreOrchestrator is Ownable {
     address public tribeFeiPair;
 
     // ----------- Time periods -----------
-    uint256 public constant RELEASE_WINDOW = 4 * 365 days;
+    uint256 public constant RELEASE_WINDOW = ROPSTEN ?  3 days : 100_000;
 
-    uint256 public constant TIMELOCK_DELAY = 1 days;
-    uint256 public constant GENESIS_DURATION = 3 days;
+    uint256 public constant TIMELOCK_DELAY = ROPSTEN ? 10 minutes : 10;
+    uint256 public constant GENESIS_DURATION = ROPSTEN ? 30 minutes : 55;
 
-    uint256 public constant POOL_DURATION = 2 * 365 days;
+    uint256 public constant POOL_DURATION =  ROPSTEN ? 3 days : 200;
     
-    uint256 public constant DRIP_FREQUENCY = 1 weeks;
+    uint256 public constant DRIP_FREQUENCY = ROPSTEN ? 10 minutes : 1;
 
-    uint256 public constant THAWING_DURATION = 2 weeks;
+    uint256 public constant THAWING_DURATION = ROPSTEN ? 4 hours : 10 minutes;
 
-    uint256 public constant UNI_ORACLE_TWAP_DURATION = 10 minutes; // 10 min twap
+    uint256 public constant UNI_ORACLE_TWAP_DURATION = ROPSTEN ? 1 minutes : 1;
 
-    uint256 public constant BONDING_CURVE_INCENTIVE_DURATION = 1 days; // 1 day duration
+    uint256 public constant BONDING_CURVE_INCENTIVE_DURATION = 10 minutes;
 
     // ----------- Params -----------
     uint256 public constant EXCHANGE_RATE_DISCOUNT = 10;
 
     uint32 public constant INCENTIVE_GROWTH_RATE = 75; // a bit over 1 unit per 5 hours assuming 13s block time
 
-    uint256 public constant SCALE = 100_000_000e18;
-    uint256 public constant BONDING_CURVE_INCENTIVE = 500e18;
+    uint256 public constant SCALE = ROPSTEN ? 100e18 : 100_000_000;
+    uint256 public constant BONDING_CURVE_INCENTIVE = ROPSTEN ? 5e18 : 5000;
 
-    uint256 public constant REWEIGHT_INCENTIVE = 500e18;
+    uint256 public constant REWEIGHT_INCENTIVE = ROPSTEN ? 5e18 : 5000;
     uint256 public constant MIN_REWEIGHT_DISTANCE_BPS = 100;
 
     bool public constant USDC_PER_ETH_IS_PRICE_0 = false;
@@ -140,6 +145,7 @@ contract CoreOrchestrator is Ownable {
         stakingOrchestrator = IStakingOrchestrator(_stakingOrchestrator);
 
         admin = _admin;
+        core.grantGovernor(_admin);
     }
 
     function initCore() public onlyOwner {
