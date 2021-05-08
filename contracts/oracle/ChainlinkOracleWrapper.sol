5b2dee9ac0a21796e3428745136db38d6763c7fb
Erwan Beauvois <erwan@allmyt.eu>
2021-05-08 17:18:38 +0200

    Add decimal correction to ChainlinkOracleWrapper.sol

--- contracts/oracle/ChainlinkOracleWrapper.sol
+++ contracts/oracle/ChainlinkOracleWrapper.sol
@@ -15,9 +15,11 @@ contract ChainlinkOracleWrapper is IOracle, CoreRef {
 
     /// @notice the referenced chainlink oracle
     AggregatorV3Interface public chainlinkOracle;
+    uint256 public oracleDecimalsNormalizer;
 
     /// @notice multiply the Chainlink oracle value by this constant, for instance for decimal corrections
     uint256 public k;
+    uint256 public BASIS_POINTS_GRANULARITY = 10_000;
 
     /// @notice should this oracle return 1 / oraclePrice instead of oraclePrice ?
     bool public reverse;
@@ -29,13 +31,20 @@ contract ChainlinkOracleWrapper is IOracle, CoreRef {
     /// @param _reverse should the price feed be inverted ?
     constructor(
         address _core,
-        AggregatorV3Interface _chainlinkOracle,
+        address _chainlinkOracle,
         uint256 _k,
         bool _reverse
     ) public CoreRef(_core) {
         chainlinkOracle = AggregatorV3Interface(_chainlinkOracle);
         k = _k;
         reverse = _reverse;
+
+        _init();
+    }
+
+    function _init() internal {
+        uint8 oracleDecimals = chainlinkOracle.decimals();
+        oracleDecimalsNormalizer = 10 ** uint256(oracleDecimals);
     }
 
     /// @notice updates the oracle price
@@ -54,15 +63,15 @@ contract ChainlinkOracleWrapper is IOracle, CoreRef {
     /// @return oracle price
     /// @return true if price is valid
     function read() external view override returns (Decimal.D256 memory, bool) {
-        (, int price,,,) = chainlinkOracle.latestRoundData();
+        (,int256 price,,,) = chainlinkOracle.latestRoundData();
         bool valid = !paused() && price > 0;
 
-        Decimal.D256 memory value = Decimal.from((uint256)(price));
-        if (k != 1) {
-          value = value.mul(k);
-        }
+        Decimal.D256 memory value = Decimal.from(uint256(price)).div(oracleDecimalsNormalizer);
         if (reverse) {
           value = Decimal.one().div(value);
+        }
+        if (k != BASIS_POINTS_GRANULARITY) {
+          value = value.mul(k).div(BASIS_POINTS_GRANULARITY);
         }
 
         return (value, valid);
