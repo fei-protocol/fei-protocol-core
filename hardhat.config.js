c32c8e3f4b51d2a0f5834b17fe51f6c754aeaf51
crypto-dev-1 <crypto-dev-1@outlook.com>
2021-05-30 21:33:21 +0100

    feat: remove docs

---
+++ hardhat.config.js
@@ -0,0 +1,37 @@
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-truffle5');
+require('hardhat-gas-reporter');
+require('hardhat-contract-sizer');
+require('solidity-coverage');
+
+require('dotenv').config();
+
+const testnetPrivateKey = process.env.TESTNET_PRIVATE_KEY;
+const rinkebyAlchemyApiKey = process.env.RINKEBY_ALCHEMY_API_KEY;
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: '0.8.4',
+  gasReporter: {
+    enabled: !!process.env.REPORT_GAS,
+  },
+  networks: {
+    hardhat: {
+      gas: 12e6,
+      allowUnlimitedContractSize: true,
+      chainId: 5777, // Any network (default: none)
+    },
+    localhost: {},
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${rinkebyAlchemyApiKey}`,
+      //   accounts: [testnetPrivateKey],
+    },
+  },
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
+  },
+};
