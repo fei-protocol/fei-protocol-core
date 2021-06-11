4fead9c0cb70418f922fafbb58cb36327a97fc2c
Sebastian <sdalcega@gmail.com>
2021-06-10 17:59:37 -0700

    remove comments

--- scripts/dao/fip_8.js
+++ scripts/dao/fip_8.js
@@ -1,20 +1,3 @@
-
-
-// 1. Unlock the admin from rari pool and transfer admin over to our timelock (see sudo script). Tetranode will do this on mainnet
-// 2. Write a script that mimicks the DAO proposal after having unlocked the timelock by executing the proposal steps on the timelock itself.
-// 3. Then run a validation script that checks balances are updated (10mil in rari pool, I have ctokens and I'm the admin on the pool)
-// 4. Replicate those proposal steps on ropsten/preview tally. This gets the call data from the exact transaction that you'll submit to the DAO. Copy paste that into my computer. Sumbit proposal, click data on metamask and copy the hex
-// 5. scripts/dao/exec.js takes in the hex and submits it straight through the DAO
-// 6. Run same validation code as before to check everything is updated
-// 7. When you submit to the dao you just copy paste the data into metamask, not on tally
-
-// Actual calls that need to be done
-// 1. Accept admin role on the rari fuse pool
-// 1.X. Change params on rari pool (???)
-// 2. Mint the FEI into timelock
-// 3. Approve Rari pool to transfer FEI (just the amount) (check function to remove the supply cap - how much you can supply for a single address)
-// 4. Trigger the mint function into the rari pool (receive c tokens in return and leave them in the timelock)
-
 const { web3 } = require('@openzeppelin/test-helpers/src/setup');
 const hre = require('hardhat');
 const CErc20Delegator = artifacts.require('CErc20Delegator');
