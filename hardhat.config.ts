36a37ed8c46abce4883148b9ddf84fde37d07032
crypto-dev-1 <crypto-dev-1@outlook.com>
2021-06-13 23:33:07 +0100

    fix: correctly pass block into config previously was block, type information suggests blockNumber

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -28,6 +28,7 @@ const config: HardhatUserConfig = {
       chainId: 5777, // Any network (default: none)
       forking: {
         url: `https://eth-mainnet.alchemyapi.io/v2/${mainnetAlchemyApiKey}`,
+        blockNumber: 12585055
       }
     },
     localhost: {
