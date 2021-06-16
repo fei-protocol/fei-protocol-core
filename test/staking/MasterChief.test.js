ee5093bd2a484ea5ca7f3e94fbc5ed5ded70596d
solsr <elliot@Elliots-MBP.lan>
2021-06-16 00:56:37 -0400

    update tests

--- test/staking/MasterChief.test.js
+++ test/staking/MasterChief.test.js
@@ -6,22 +6,22 @@ const {
   getCore,
   getAddresses,
 } = require('../helpers');
+const { time } = require('@openzeppelin/test-helpers');
 
 const Tribe = artifacts.require('MockTribe');
 const MockCoreRef = artifacts.require('MockCoreRef');
 const MasterChief = artifacts.require('MasterChief');
 const MockERC20 = artifacts.require('MockERC20');
+const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 
 describe('MasterChief', function () {
   let userAddress;
   let minterAddress;
-  let burnerAddress;
-  let pcvControllerAddress;
   let governorAddress;
-  let genesisGroup;
-  let guardianAddress;
-  let LPToken;
   const allocationPoints = 100;
+  const totalStaked = '100000000000000000000';
+  const perBlockReward = Number(100000000000000000000);
+  let pid;
 
   beforeEach(async function () {
     ({
@@ -45,8 +45,10 @@ describe('MasterChief', function () {
     await this.LPToken.mint(userAddress, '1000000000000000000000000000000000000000000000');
     await this.tribe.mint(this.masterChief.address, '1000000000000000', { from: minterAddress });
     // create new reward stream
-    await this.masterChief.add(allocationPoints, this.LPToken.address, this.tribe.address, { from: governorAddress });
-
+    const tx = await this.masterChief.add(allocationPoints, this.LPToken.address, ZERO_ADDRESS, { from: governorAddress });
+    
+    pid = Number(tx.logs[0].args.pid);
+    // console.log("pid: ", pid)
     this.minterRole = await this.core.MINTER_ROLE();
     this.burnerRole = await this.core.BURNER_ROLE();
     this.governorRole = await this.core.GOVERN_ROLE();
@@ -91,21 +93,47 @@ describe('MasterChief', function () {
     });
   });
 
-//   describe('Fei Update', function() {
-//     it('updates', async function() {
-//       expectEvent(
-//         await this.core.setFei(userAddress, {from: governorAddress}),
-//         'FeiUpdate',
-//         {
-//           _fei: userAddress
-//         }
-//       );
-//       expect(await this.core.fei()).to.be.equal(userAddress);
-//     });
-	
-//     it('non governor reverts', async function() {
-//       await expectRevert(this.core.setFei(userAddress, {from: userAddress}), 'Permissions: Caller is not a governor');
-//     });
-//   });
+  describe('Test Staking', function() {
+    it('should be able to stake LP Tokens', async function() {
+        await this.LPToken.approve(this.masterChief.address, totalStaked);
+        await this.masterChief.deposit(pid, totalStaked, userAddress, { from: userAddress })
+    });
 
+    it('should be able to get pending sushi', async function() {
+        await this.LPToken.approve(this.masterChief.address, totalStaked);
+        await this.masterChief.deposit(pid, totalStaked, userAddress, { from: userAddress })
+
+        const advanceBlockAmount = 100;
+        for (let i = 0; i < advanceBlockAmount; i++) {
+            await time.advanceBlock();
+        }
+
+        expect(Number(await this.masterChief.pendingSushi(pid, userAddress))).to.be.equal(perBlockReward * advanceBlockAmount);
+    });
+
+    it('should be able to get pending sushi after one block with a single pool and user staking', async function() {
+        await this.LPToken.approve(this.masterChief.address, totalStaked);
+        await this.masterChief.deposit(pid, totalStaked, userAddress, { from: userAddress })
+
+        await time.advanceBlock();
+
+        expect(Number(await this.masterChief.pendingSushi(pid, userAddress))).to.be.equal(perBlockReward);
+    });
+
+    it('should be able to get pending sushi 2x', async function() {
+        await this.LPToken.approve(this.masterChief.address, totalStaked);
+        await this.masterChief.deposit(pid, totalStaked, userAddress, { from: userAddress })
+
+        const advanceBlockAmount = 200;
+        for (let i = 0; i < advanceBlockAmount; i++) {
+            await time.advanceBlock();
+        }
+
+        expect(Number(await this.masterChief.pendingSushi(pid, userAddress))).to.be.equal(perBlockReward * advanceBlockAmount);
+    });
+
+    it('should be able to assert poolLength', async function() {
+        expect(Number(await this.masterChief.poolLength())).to.be.equal(1);
+    });
+  });
 });
