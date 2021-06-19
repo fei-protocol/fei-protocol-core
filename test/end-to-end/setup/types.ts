1885baf3f0dd5893f96bdf619b6383be1c11b668
crypto-dev-1 <crypto-dev-1@outlook.com>
2021-06-19 17:58:36 +0100

    refactor: restructure e2e setup dir and break out interface + types

---
+++ test/end-to-end/setup/types.ts
@@ -0,0 +1,33 @@
+export type TestEnv = {
+  contracts: any[]
+  addresses: any[]
+}
+
+export interface TestCoordinator {
+  initialiseLocalEnv(): Promise<TestEnv>;
+  initialiseMainnetEnv(): Promise<TestEnv>;
+}
+
+export type ContractAddresses = {
+  Core: string,
+  Tribe: string,
+  FeiDAO: string,
+  FeiDAOTimelock: string,
+  Fei: string,
+  UniswapIncentive: string,
+  EthBondingCurve: string,
+  EthUniswapPCVDeposit: string,
+  EthUniswapPCVController: string,
+  UniswapOracle: string,
+  BondingCurveOracle: string,
+  FeiRewardsDistributor: string,
+  FeiStakingRewards: string,
+  GenesisGroup: string,
+  FeiRouter: string,
+  EthReserveStabiliser: string,
+  EthPCVDripper: string,
+  RatioPCVController: string,
+  EthPCVDepositAdapter: string,
+  FEIETHUniV2Pair: string,
+  FEITRIBEUniV2Pair: string
+}
