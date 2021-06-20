b8fae30fed38a872fb6969c0e3d41dfe67bd9305
crypto-dev-1 <crypto-dev-1@outlook.com>
2021-06-20 23:03:01 +0100

    refactor: break e2e tests out into seperate dir

--- end-to-end/e2e.spec.ts
+++ end-to-end/e2e.spec.ts
@@ -42,21 +42,39 @@ describe('e2e', function () {
   })
 
   it('should transfer allocation from bonding curve to the reserve stabiliser', async function () {
-    
+    // 1. Make sure there is ETH in bonding curve
+    // 2. Get the Eth balances of the pcvDeposit allocation (class PCVSplitter)
+    // Check duration window to see if passed (potentially fast-forward to ensure passed)
+    // 3. trigger allocate
+    // 4. Make sure ETH leaves bonding curve and goes to allocation 
+    // When checking balances use the .balance()
+    // (possible amount received != sent if allocation includes EthUniswapPCVDeposit)
+    // 5. Make sure caller receives FEI incentive if duration had passed
   })
 
   it('should be able to redeem Fei from stabiliser', async function () {
-
+    //         uint256 adjustedAmountIn = amountFeiIn * usdPerFeiBasisPoints / BASIS_POINTS_GRANULARITY;
+    //        return invert(readOracle()).mul(adjustedAmountIn).asUint256();
   })
 
   it('should perform reweight above peg correctly',  async function () {
-
+    // /scripts/validation/upgrade.js
   })
 
   it('should perform reweight below peg correctly',  async function () {
-    
+    // /scripts/validation/upgrade.js
   })
 
   it('drip controller can withdraw from PCV deposit to stabiliser', async function () {
+    // 1. drain stabilizer
+    // 2. Trigger drip
+    // 3. check PCV deposit loses dripAmount ETH and stabilizer gets dripAmount ETH
+    // 4. check caller receives FEI mint
+  })
+
+  it('assert all access control', async function () {
+    // make sure all roles that have access do
+    // make sure no extra roles have access (perhaps just check cardinality of each role  `core.getRoleMemberCount(role bytes)`)
+    // core.BURNER_ROLE() - returns bytes32 role id
   })
 });
---
+++ end-to-end/setup/index.ts
@@ -0,0 +1,126 @@
+import mainnetAddressesV1 from '../../contract-addresses/mainnetAddresses.json'
+import { getContracts, getContract } from './loadContracts'
+import { ContractAddresses, TestCoordinator, TestEnv, TestEnvContracts } from './types'
+import { sudo } from '../../scripts/utils/sudo'
+import { upgrade as upgradeProtocol } from '../../deploy/upgrade'
+import { upgrade as applyPermissions } from '../../scripts/dao/upgrade'
+
+/**
+ * Coordinate initialising an end-to-end testing environment
+ * Able to run with additional local contracts deployed or
+ * in a purely 100% mainnet forked mode
+*/
+export class TestEndtoEndCoordinator implements TestCoordinator { 
+  private supportedNetworks = ['mainnet', 'local']
+  private addresses: ContractAddresses;
+
+  constructor(
+    private network: string,
+    private deployAddress: string,
+    private logging: boolean,
+    private version: number
+  ) {
+    if (this.supportedNetworks.includes(this.network)) {
+      this.addresses = this.getContractAddressesForNetwork(network)
+    } else {
+      throw new Error('Unsupported network')
+    }
+  }
+
+  /**
+   * Setup end-to-end tests for a 100% forked mainnet environment.
+   * No additional contracts deployed locally. This test is used to e2e the real system.
+   * Specifically:
+   * 1) Load all mainnet contracts from their addresses
+   * 2) Get accounts ready to execute tests from
+   */
+  async initialiseMainnetEnv(): Promise<TestEnv> {
+    const contracts = await this.loadMainnetContracts(this.addresses)
+    return { contracts }
+  }
+
+  /**
+   * Setup end to end tests for a local environment. This involves e2e testing
+   * contracts not yet deployed.
+   * Specifically:
+   * 1) Deploy contracts that are only in the codebase and not yet on Mainnet
+   * 2) Grant governor access
+   * 3) Apply appropriate permissions to the contracts
+   * 
+   * Note: This is running on a forked mainnet state to account for 
+   * the various dependencies in Uniswap, Chainlink etc.
+   * 
+   * TODO: add in an override where you can choose which contracts to deploy
+   * and upgrade
+   */
+   public async initialiseLocalEnv(): Promise<TestEnv> {
+    //  upgradedContracts = {'core': '0x124'}
+    // applies overrride 
+    const contracts = await this.getProtocolContracts()
+
+    const requiredSudoAddresses = {
+      coreAddress: this.addresses['core'],
+      feiAddress: this.addresses['fei'],
+      timelockAddress: this.addresses['timelock']
+    }
+    await sudo(requiredSudoAddresses, this.logging)
+    
+    // if contract not upgraded, use mainnet version. this.addresses['core']
+    // if contract is upgraded, take in override and deploy new contract +
+    // get address contracts.core.address
+    const requiredApplyPermissionsAddresses = {
+      coreAddress: this.addresses['core'],
+      ethUniswapPCVDepositAddress: contracts.uniswapPCVController.address,
+      ethUniswapPCVControllerAddress: contracts.uniswapPCVController.address,
+      ethBondingCurveAddress: contracts.bondingCurve.address,
+      ethReserveStabilizerAddress: contracts.ethReserveStabilizer.address,
+      ratioPCVControllerAddress: contracts.ratioPCVController.address,
+      pcvDripControllerAddress: contracts.pcvDripController.address,
+      ethPairAddress: this.addresses['feiEthPair'],
+      timelockAddress: this.addresses['timelock'],
+      tribeReserveStabilizerAddress: contracts.tribeReserveStabilizer.address
+    };
+
+    await applyPermissions(requiredApplyPermissionsAddresses, this.logging)
+    return { contracts }
+  }
+  
+  /**
+   * Get all Mainnet contracts, instantiated as web3 instances
+   */
+  async loadMainnetContracts(addresses: ContractAddresses): Promise<TestEnvContracts> {
+    return getContracts(addresses)
+  }
+
+  /**
+   * Load all contract addresses from a .json, according to the network configured
+   */
+  private getContractAddressesForNetwork(network: string): ContractAddresses {
+    if (this.supportedNetworks.includes(network)) {
+      return mainnetAddressesV1
+    } else {
+      throw new Error('No addresses for this network')
+    } 
+  }
+
+  /**
+   * Deploy the contracts that exist in the protocol locally and 
+   * which are not yet on Mainnet
+   */
+  private async getProtocolContracts(): Promise<TestEnvContracts> {
+    const core = await getContract('core', this.addresses['core'])
+    const fei = await getContract('fei', this.addresses['fei'])
+    const tribe = await getContract('tribe', this.addresses['tribe'])
+    const ethReserveStabilizer = await getContract('ethReserveStabilizer', this.addresses['ethReserveStabilizer'])
+
+    const configAddresses = {
+      coreAddress: this.addresses['core'],
+      feiEthPairAddress: this.addresses['feiEthPair'],
+      wethAddress: this.addresses['weth'],
+      uniswapRouterAddress: this.addresses['uniswapRouter'],
+      uniswapOracleAddress: this.addresses['uniswapOracle']
+    }
+    const upgradeContracts = await upgradeProtocol(this.deployAddress, configAddresses, this.logging)
+    return { core, fei, tribe, ethReserveStabilizer, ...upgradeContracts}
+  }
+}
