5513af813f650914487cfe00215640bb7e5ad8f4
Joey Santoro <joseph.santoro@duke.edu>
2021-07-09 17:43:14 -0700

    solidity v0.8.4

--- contracts/oracle/UniswapOracle.sol
+++ contracts/oracle/UniswapOracle.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 // Referencing Uniswap Example Simple Oracle
 // https://github.com/Uniswap/uniswap-v2-periphery/blob/master/contracts/examples/ExampleOracleSimple.sol
