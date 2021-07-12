3c55d72aaf6f1f60850165a7a9b2e4d59c380551
Joey Santoro <joseph.santoro@duke.edu>
2021-07-11 21:05:28 -0700

    add e2e test for Compound PCV Deposit

--- proposals/dao/compoundPCVDeposit.js
+++ proposals/dao/compoundPCVDeposit.js
@@ -1,24 +1,29 @@
-const hre = require('hardhat');
+const IERC20 = artifacts.require('IERC20');
 
-const { constants: { ZERO_ADDRESS } } = require('@openzeppelin/test-helpers');
-
-const EthCompoundPCVDeposit = artifacts.require('EthCompoundPCVDeposit');
-const ERC20CompoundPCVDeposit = artifacts.require('ERC20CompoundPCVDeposit');
-
-const { web3 } = hre;
 const e18 = '000000000000000000';
 
-async function setup(addresses, oldContractAddresses, logging) {}
+async function setup(addresses, oldContracts, contracts, logging) {}
 
 // The DAO steps for FIP-9, these must be done with Governor access control privileges
-async function run(addresses, oldContractAddresses, logging = false) {
-  const accounts = await web3.eth.getAccounts();
+async function run(addresses, oldContracts, contracts, logging = false) {
   const {
-    rariPool8EthPCVDepositAddress,
-    rariPool8FeiPCVDepositAddress
+    rariPool8FeiAddress,
+    timelockAddress
   } = addresses;
-  const dripper = await EthCompoundPCVDeposit.at(rariPool8EthPCVDepositAddress);
-  const steth = await ERC20CompoundPCVDeposit.at(rariPool8FeiPCVDepositAddress);
+
+  const {
+    rariPool8FeiPCVDeposit,
+    rariPool8EthPCVDeposit,
+    ethPCVDripper
+  } = contracts;
+  const rariPool8Fei = await IERC20.at(rariPool8FeiAddress);
+  const balanceOfRariFei = await rariPool8Fei.balanceOf(timelockAddress);
+
+  await rariPool8Fei.transfer(rariPool8FeiPCVDeposit.address, balanceOfRariFei, {from: timelockAddress});
+
+  await ethPCVDripper.withdrawETH(rariPool8EthPCVDeposit.address, `1000${e18}`);
+  
+  await rariPool8EthPCVDeposit.deposit();
 }
 
 async function teardown(addresses, oldContractAddresses, logging) {}
