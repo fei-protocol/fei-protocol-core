0f9da8ccb0a77908d35c3877107c4b1c0dfd3b59
Joey Santoro <joseph.santoro@duke.edu>
2021-08-10 22:50:14 -0700

    hardhat block

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -29,7 +29,7 @@ const config: HardhatUserConfig = {
       chainId: 5777, // Any network (default: none)
       forking: {
         url: `https://eth-mainnet.alchemyapi.io/v2/${mainnetAlchemyApiKey}`,
-        blockNumber: 12974210
+        blockNumber: 12994673
       }
     },
     localhost: {
