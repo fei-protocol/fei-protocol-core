9664127e86b0912aa630630ed0a48c18f1a67d30
Joey Santoro <joseph.santoro@duke.edu>
2021-08-16 15:04:15 -0700

    ICollateralizationOracle

---
+++ contracts/oracle/ICollateralizationOracle.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.4;
+
+import "./IOracle.sol";
+
+/// @title Collateralization ratio oracle interface for Fei Protocol
+/// @author Fei Protocol
+interface ICollateralizationOracle is IOracle {
+
+    // ----------- Getters -----------
+
+    function isOvercollateralized() external view returns (bool);
+
+    function userCirculatingFei() external view returns (uint256);
+
+    function pcvValue() external view returns (uint256);
+
+    function pcvEquityValue() external view returns (uint256); 
+}
