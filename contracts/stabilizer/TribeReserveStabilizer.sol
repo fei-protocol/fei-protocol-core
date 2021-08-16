46071e38a2d898a0b96df72b78219f785d2c4377
Joey Santoro <joseph.santoro@duke.edu>
2021-08-16 15:59:29 -0700

    tribe reserve stabilizer

--- contracts/stabilizer/TribeReserveStabilizer.sol
+++ contracts/stabilizer/TribeReserveStabilizer.sol
@@ -14,37 +14,37 @@ interface ITribe is IERC20 {
 contract TribeReserveStabilizer is ITribeReserveStabilizer, ReserveStabilizer {
     using Decimal for Decimal.D256;
 
-    /// @notice a FEI oracle reporting USD per FEI
-    IOracle public override feiOracle;
+    /// @notice a collateralization oracle
+    ICollateralizationOracle public override collateralizationOracle;
 
-    Decimal.D256 private _feiPriceThreshold;
+    Decimal.D256 private _collateralizationThreshold;
 
     /// @notice Tribe Reserve Stabilizer constructor
     /// @param _core Fei Core to reference
     /// @param _tribeOracle the TRIBE price oracle to reference
     /// @param _backupOracle the backup oracle to reference
     /// @param _usdPerFeiBasisPoints the USD price per FEI to sell TRIBE at
-    /// @param _feiOracle the FEI price oracle to reference
-    /// @param _feiPriceThresholdBasisPoints the FEI price below which the stabilizer becomes active. Reported in basis points (1/10000)
+    /// @param _collateralizationOracle the collateralization oracle to reference
+    /// @param _collateralizationThresholdBasisPoints the collateralization ratio below which the stabilizer becomes active. Reported in basis points (1/10000)
     constructor(
         address _core,
         address _tribeOracle,
         address _backupOracle,
         uint256 _usdPerFeiBasisPoints,
-        IOracle _feiOracle,
-        uint256 _feiPriceThresholdBasisPoints
+        ICollateralizationOracle _collateralizationOracle,
+        uint256 _collateralizationThresholdBasisPoints
     ) ReserveStabilizer(_core, _tribeOracle, _backupOracle, IERC20(address(0)), _usdPerFeiBasisPoints) {
-        feiOracle = _feiOracle;
-        emit FeiOracleUpdate(address(0), address(_feiOracle));
+        collateralizationOracle = _collateralizationOracle;
+        emit CollateralizationOracleUpdate(address(0), address(_collateralizationOracle));
     
-        _feiPriceThreshold = Decimal.ratio(_feiPriceThresholdBasisPoints, BASIS_POINTS_GRANULARITY);
-        emit FeiPriceThresholdUpdate(0, _feiPriceThresholdBasisPoints);
+        _collateralizationThreshold = Decimal.ratio(_collateralizationThresholdBasisPoints, BASIS_POINTS_GRANULARITY);
+        emit CollateralizationThresholdUpdate(0, _collateralizationThresholdBasisPoints);
     }
 
     /// @notice exchange FEI for minted TRIBE
-    /// @dev FEI oracle price must be below threshold
+    /// @dev Collateralization oracle price must be above threshold
     function exchangeFei(uint256 feiAmount) public override returns(uint256) {
-        require(isFeiBelowThreshold(), "TribeReserveStabilizer: FEI price too high");
+        require(isCollateralizationAboveThreshold(), "TribeReserveStabilizer: Collateralization ratio too low");
         return super.exchangeFei(feiAmount);
     }
 
@@ -58,34 +58,32 @@ contract TribeReserveStabilizer is ITribeReserveStabilizer, ReserveStabilizer {
         return tribeBalance();
     }
 
-    /// @notice check whether FEI price is below the threshold set relative to USD
-    /// @dev returns false if the FEI oracle is invalid
-    function isFeiBelowThreshold() public override view returns(bool) {
-        (Decimal.D256 memory price, bool valid) = feiOracle.read();
+    /// @notice check whether collateralization ratio is above the threshold set
+    /// @dev returns false if the oracle is invalid
+    function isCollateralizationAboveThreshold() public override view returns(bool) {
+        (Decimal.D256 memory price, bool valid) = collateralizationOracle.read();
 
-        return valid && price.lessThanOrEqualTo(_feiPriceThreshold);
+        return valid && price.lessThanOrEqualTo(_collateralizationThreshold);
     }
 
-    /// @notice set the FEI oracle
-    function setFeiOracle(IOracle newFeiOracle) external override onlyGovernor {
-        require(address(newFeiOracle) != address(0), "TribeReserveStabilizer: zero address");
-        address oldFeiOracle = address(feiOracle);
-        feiOracle = newFeiOracle;
-        emit FeiOracleUpdate(oldFeiOracle, address(newFeiOracle));
+    /// @notice set the Collateralization oracle
+    function setCollateralizationOracle(ICollateralizationOracle newCollateralizationOracle) external override onlyGovernor {
+        require(address(newCollateralizationOracle) != address(0), "TribeReserveStabilizer: zero address");
+        address oldCollateralizationOracle = address(collateralizationOracle);
+        collateralizationOracle = newCollateralizationOracle;
+        emit CollateralizationOracleUpdate(oldCollateralizationOracle, address(newCollateralizationOracle));
     }
     
-    /// @notice set the FEI price threshold below which exchanging becomes active
-    function setFeiPriceThreshold(uint256 newFeiPriceThresholdBasisPoints) external override onlyGovernor {
-        require(newFeiPriceThresholdBasisPoints <= BASIS_POINTS_GRANULARITY, "TribeReserveStabilizer: fei price threshold too high");
-        
-        uint256 oldFeiPriceThresholdBasisPoints = _feiPriceThreshold.mul(BASIS_POINTS_GRANULARITY).asUint256();
-        _feiPriceThreshold = Decimal.ratio(newFeiPriceThresholdBasisPoints, BASIS_POINTS_GRANULARITY);
-        emit FeiPriceThresholdUpdate(oldFeiPriceThresholdBasisPoints, newFeiPriceThresholdBasisPoints);
+    /// @notice set the collateralization threshold below which exchanging becomes active
+    function setCollateralizationThreshold(uint256 newCollateralizationThresholdBasisPoints) external override onlyGovernor {        
+        uint256 oldCollateralizationThresholdBasisPoints = _collateralizationThreshold.mul(BASIS_POINTS_GRANULARITY).asUint256();
+        _collateralizationThreshold = Decimal.ratio(newCollateralizationThresholdBasisPoints, BASIS_POINTS_GRANULARITY);
+        emit CollateralizationThresholdUpdate(oldCollateralizationThresholdBasisPoints, newCollateralizationThresholdBasisPoints);
     }
 
-    /// @notice the FEI price threshold below which exchanging becomes active
-    function feiPriceThreshold() external view override returns(Decimal.D256 memory) {
-        return _feiPriceThreshold;
+    /// @notice the collateralization threshold below which exchanging becomes active
+    function collateralizationThreshold() external view override returns(Decimal.D256 memory) {
+        return _collateralizationThreshold;
     }
 
     /// @notice mints TRIBE to the target address
