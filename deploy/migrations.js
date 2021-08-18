d945c053d5057073f5c1f3de5408a63ddd8c9623
Joey Santoro <joseph.santoro@duke.edu>
2021-08-18 15:02:20 -0700

    deploy script update

--- deploy/migrations.js
+++ deploy/migrations.js
@@ -1,10 +1,15 @@
 import mainnetAddressesV1 from '../contract-addresses/mainnetAddresses.json';
-import proposals from '../proposals/config.json';
 
 const { web3 } = require('hardhat');
 
-// Run the deployments flagged as deploy=true in '../proposals/config.json'
+// Run the deployment for DEPLOY_FILE
 async function main() {
+  const proposalName = process.env.DEPLOY_FILE;
+
+  if (!proposalName) {
+    throw new Error('DEPLOY_FILE env variable not set');
+  }
+
   const deployAddress = (await web3.eth.getAccounts())[0];
 
   const mainnetAddresses = {};
@@ -13,15 +18,8 @@ async function main() {
     return true;
   });
 
-  const proposalNames = Object.keys(proposals);
-  for (let i = 0; i < proposalNames.length; i += 1) {
-    const proposalName = proposalNames[i];
-    const config = proposals[proposalNames[i]];
-    if (config.deploy) {
-      const { deploy } = await import(`./${proposalName}`);
-      await deploy(deployAddress, mainnetAddresses, true);
-    }
-  }
+  const { deploy } = await import(`./${proposalName}`);
+  await deploy(deployAddress, mainnetAddresses, true);
 }
 
 main()
