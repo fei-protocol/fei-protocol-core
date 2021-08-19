f6db6d6ad6aa6c04f1279b682adff4ee4caecec7
Joey Santoro <joseph.santoro@duke.edu>
2021-08-19 14:55:33 -0700

    validation

--- proposals/dao/fip_13b.js
+++ proposals/dao/fip_13b.js
@@ -1,3 +1,4 @@
+import { expect } from 'hardhat';
 import { BN, expectApprox } from '../../test/helpers';
 
 const e18 = '000000000000000000';
@@ -67,11 +68,29 @@ async function validate(addresses, oldContracts, contracts) {
     rariPool27FeiPCVDeposit
   } = contracts;
 
+  const {
+    feiAddress,
+    rariPool9FeiAddress,
+    rariPool25FeiAddress,
+    rariPool26FeiAddress,
+    rariPool27FeiAddress,
+  } = addresses;
+
   const millionFei = new BN(`1000000${e18}`);
   expectApprox(await rariPool9FeiPCVDeposit.balance(), millionFei);
   expectApprox(await rariPool25FeiPCVDeposit.balance(), millionFei);
   expectApprox(await rariPool26FeiPCVDeposit.balance(), millionFei);
   expectApprox(await rariPool27FeiPCVDeposit.balance(), millionFei);
+
+  expect(await rariPool9FeiPCVDeposit.token()).to.be.equal(feiAddress);
+  expect(await rariPool25FeiPCVDeposit.token()).to.be.equal(feiAddress);
+  expect(await rariPool26FeiPCVDeposit.token()).to.be.equal(feiAddress);
+  expect(await rariPool27FeiPCVDeposit.token()).to.be.equal(feiAddress);
+
+  expect(await rariPool9FeiPCVDeposit.cToken()).to.be.equal(rariPool9FeiAddress);
+  expect(await rariPool25FeiPCVDeposit.cToken()).to.be.equal(rariPool25FeiAddress);
+  expect(await rariPool26FeiPCVDeposit.cToken()).to.be.equal(rariPool26FeiAddress);
+  expect(await rariPool27FeiPCVDeposit.cToken()).to.be.equal(rariPool27FeiAddress);
 }
 
 module.exports = {
