a31fe0b9953356089382094ca786997439701fe2
Joey Santoro <joseph.santoro@duke.edu>
2021-09-26 10:43:57 -0700

    ts

---
+++ scripts/deploy/governorUpgrade.ts
@@ -0,0 +1,28 @@
+import { DeployUpgradeFunc } from '../../test/integration/setup/types';
+import { ethers } from 'hardhat';
+
+const deploy: DeployUpgradeFunc = async (deployAddress, addresses, logging = false) => {
+
+  const {
+    tribe,
+    timelock
+  } = addresses;
+
+  if (
+    !tribe || !timelock
+  ) {
+    throw new Error('An environment variable contract address is not set');
+  }
+
+  const feiDAO = await (
+    await ethers.getContractFactory('FeiDAO')
+  ).deploy(tribe, timelock);
+
+  logging && console.log('FeiDAO deployed to: ', feiDAO.address);
+
+  return {
+    feiDAO
+  };
+};
+
+module.exports = { deploy };
