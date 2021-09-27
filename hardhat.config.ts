39cdd845ae2de46f1577e565eb3817d65db64d43
Caleb <ditchfieldcaleb@gmail.com>
2021-09-26 23:05:40 -0700

    move types to types

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -106,7 +106,7 @@ export default {
   },
 
   typechain: {
-    outDir: './types',
+    outDir: './types/contracts/',
     target: 'ethers-v5',
     alwaysGenerateOverloads: false // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
   },
--- scripts/deploy/optimisticTimelock.ts
+++ scripts/deploy/optimisticTimelock.ts
@@ -1,18 +1,18 @@
-import { DeployUpgradeFunc } from '../../test/integration/setup/types';
+import { DeployUpgradeFunc } from '../../types/types';
 import { ethers } from 'hardhat';
 
 const fourDays = 4 * 24 * 60 * 60;
 
 const deploy: DeployUpgradeFunc = async (deployAddress, addresses, logging = false) => {
-  const { tribalChiefOptimisticMultisigAddress, coreAddress } = addresses;
+  const { tribalChiefOptimisticMultisig, core } = addresses;
 
-  if (!tribalChiefOptimisticMultisigAddress || !coreAddress) {
+  if (!tribalChiefOptimisticMultisig || !core) {
     throw new Error('An environment variable contract address is not set');
   }
 
   const optimisticTimelock = await (
     await ethers.getContractFactory('OptimisticTimelock')
-  ).deploy(coreAddress, fourDays, [tribalChiefOptimisticMultisigAddress], [tribalChiefOptimisticMultisigAddress]);
+  ).deploy(core.address, fourDays, [tribalChiefOptimisticMultisig.address], [tribalChiefOptimisticMultisig.address]);
 
   logging && console.log('Optimistic Timelock deployed to: ', optimisticTimelock.address);
   return {
