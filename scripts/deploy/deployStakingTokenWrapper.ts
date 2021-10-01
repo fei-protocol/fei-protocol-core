5415572c90561e988f820bfbe94568b6ee487369
Elliot <elliotfriedman3@gmail.com>
2021-10-01 15:12:07 -0700

    update staking token wrapper deploy script

--- scripts/deploy/deployStakingTokenWrapper.ts
+++ scripts/deploy/deployStakingTokenWrapper.ts
@@ -1,15 +1,11 @@
-/* eslint-disable max-len */
-
 import { ethers } from 'hardhat';
+import { DeployUpgradeFunc } from '../../types/types';
 
-async function deploy(deployAddress, addresses, logging = false) {
+export const deploy: DeployUpgradeFunc = async (deployAddress, addresses, logging = false) => {
 
-  const rariBeneficiary = '0x73F16f0c0Cd1A078A54894974C5C054D8dC1A3d7';
-
-  const { tribalChief } = addresses;
+  const { tribalChief, rariRewardsDistributorDelegator } = addresses;
   const stakingTokenWrapperFactory = await ethers.getContractFactory('StakingTokenWrapper');
-  console.log("tribalChief", tribalChief)
-  const stakingTokenWrapper = await stakingTokenWrapperFactory.deploy(tribalChief, rariBeneficiary);
+  const stakingTokenWrapper = await stakingTokenWrapperFactory.deploy(tribalChief, rariRewardsDistributorDelegator);
 
   logging && console.log('StakingTokenWrapper impl deployed to: ', stakingTokenWrapper.address);
 
@@ -17,5 +13,3 @@ async function deploy(deployAddress, addresses, logging = false) {
     stakingTokenWrapper,
   };
 }
-
-module.exports = { deploy };
