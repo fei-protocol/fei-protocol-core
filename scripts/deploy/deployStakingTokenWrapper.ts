6cf78321bccf8fed43891e980f069653ace6356c
Elliot <elliotfriedman3@gmail.com>
2021-10-01 17:10:30 -0700

    run lint

--- scripts/deploy/deployStakingTokenWrapper.ts
+++ scripts/deploy/deployStakingTokenWrapper.ts
@@ -2,7 +2,6 @@ import { ethers } from 'hardhat';
 import { DeployUpgradeFunc } from '../../types/types';
 
 export const deploy: DeployUpgradeFunc = async (deployAddress, addresses, logging = false) => {
-
   const { tribalChief, rariRewardsDistributorDelegator } = addresses;
   const stakingTokenWrapperFactory = await ethers.getContractFactory('StakingTokenWrapper');
   const stakingTokenWrapper = await stakingTokenWrapperFactory.deploy(tribalChief, rariRewardsDistributorDelegator);
@@ -10,6 +9,6 @@ export const deploy: DeployUpgradeFunc = async (deployAddress, addresses, loggin
   logging && console.log('StakingTokenWrapper impl deployed to: ', stakingTokenWrapper.address);
 
   return {
-    stakingTokenWrapper,
+    stakingTokenWrapper
   };
-}
+};
