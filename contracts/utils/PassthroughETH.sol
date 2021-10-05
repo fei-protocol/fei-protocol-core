6854f4dbf702717b3788cd26d70d17cbd404050d
Caleb Ditchfield <ditchfieldcaleb@gmail.com>
2021-10-05 13:19:07 -0700

    require success on low-level calls

--- contracts/utils/PassthroughETH.sol
+++ contracts/utils/PassthroughETH.sol
@@ -5,7 +5,8 @@ import "../pcv/IPCVDeposit.sol";
 contract AavePassthroughETH {
     address payable constant target = payable(0x5B86887e171bAE0C2C826e87E34Df8D558C079B9);
     function deposit(uint256 _amount) external payable {
-        target.call{value: address(this).balance}("");
+        (bool success, ) = target.call{value: address(this).balance}("");
+        require(success, "Address: unable to send value, recipient may have reverted");
         IPCVDeposit(target).deposit();
     }
 }
@@ -13,7 +14,8 @@ contract AavePassthroughETH {
 contract CompoundPassthroughETH {
     address payable constant target = payable(0x4fCB1435fD42CE7ce7Af3cB2e98289F79d2962b3);
     function deposit(uint256 _amount) external payable {
-        target.call{value: address(this).balance}("");
+        (bool success, ) = target.call{value: address(this).balance}("");
+        require(success, "Address: unable to send value, recipient may have reverted");
         IPCVDeposit(target).deposit();
     }
 }
