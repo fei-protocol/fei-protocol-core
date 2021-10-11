d675cd8298354b1de87d873b65b7b8b9c3a37d75
Joey Santoro <joseph.santoro@duke.edu>
2021-10-11 16:31:50 -0700

    fuse pool deploy

---
+++ scripts/deploy/compoundPCVDeposit.ts
@@ -0,0 +1,24 @@
+import { ethers } from 'hardhat';
+
+export async function deploy(deployAddress, addresses, logging = false) {
+  const { core, fei } = addresses;
+
+  const CTOKEN = process.env.CTOKEN;
+
+  if (!CTOKEN) {
+    throw new Error('CTOKEN environment variable contract address is not set');
+  }
+
+  if (!core || !fei) {
+    throw new Error('An environment variable contract address is not set');
+  }
+
+  const erc20CompoundPCVDepositFactory = await ethers.getContractFactory('ERC20CompoundPCVDeposit');
+  const erc20CompoundPCVDeposit = await erc20CompoundPCVDepositFactory.deploy(
+    core, 
+    CTOKEN, 
+    fei
+  );
+  
+  logging && console.log('EthCompoundPCVDeposit deployed to: ', erc20CompoundPCVDeposit.address);
+}
