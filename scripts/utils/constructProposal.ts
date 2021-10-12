b5b1f429356c603fd9bde25aef50ced8c9dfd7aa
Joey Santoro <joseph.santoro@duke.edu>
2021-10-11 19:20:55 -0700

    string interpolation in dao json

--- scripts/utils/constructProposal.ts
+++ scripts/utils/constructProposal.ts
@@ -1,6 +1,8 @@
-import { getAllContracts } from '../../test/integration/setup/loadContracts';
+import { getAllContractAddresses, getAllContracts } from '../../test/integration/setup/loadContracts';
 import fs from 'fs';
 import { proposals } from 'hardhat';
+import { MainnetContracts, NamedAddresses } from '@custom-types/types';
+import format from 'string-template';
 
 /**
  * Constucts a hardhat proposal object
@@ -14,7 +16,9 @@ export default async function constructProposal(proposalName: string, logging = 
   const proposalInfo = await import(`../../proposals/description/${proposalName}`);
   const proposalDescription = fs.readFileSync(`${__dirname}/../../proposals/description/${proposalName}.txt`);
 
-  const contracts = await getAllContracts();
+  const contracts: MainnetContracts = await getAllContracts();
+  const contractAddresses: NamedAddresses = await getAllContractAddresses();
+
   const proposalBuilder = proposals.builders.alpha();
   proposalBuilder.maxActions = 40;
 
@@ -22,7 +26,8 @@ export default async function constructProposal(proposalName: string, logging = 
     const command = proposalInfo.proposal_commands[i];
     const ethersContract = contracts[command.target];
 
-    proposalBuilder.addContractAction(ethersContract, command.method, command.arguments, command.values);
+    const args = replaceArgs(command.arguments, contractAddresses);
+    proposalBuilder.addContractAction(ethersContract, command.method, args, command.values);
 
     logging && console.log(`Adding proposal step: ${command.description}`);
   }
@@ -32,4 +37,21 @@ export default async function constructProposal(proposalName: string, logging = 
   const proposal = proposalBuilder.build();
   logging && console.log(await proposal.printProposalInfo());
   return proposal;
+}
+
+// Recursively interpolate strings in the argument array
+const replaceArgs = (args: any[], contractNames: NamedAddresses) => {
+  const result = [];
+  for (let i = 0; i < args.length; i++) {
+    const element = args[i];
+    if (typeof element === typeof '') {
+      const formatted = format(element, contractNames);
+      result.push(formatted);
+    } else if (typeof element === typeof []) {
+      result.push(replaceArgs(element, contractNames));
+    } else {
+      result.push(element);
+    }
+  }
+  return result;
 }
