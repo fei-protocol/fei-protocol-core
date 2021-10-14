9646c4b8520ea9dfa73d268988205dd532f02e7a
Joey Santoro <joseph.santoro@duke.edu>
2021-10-13 23:02:11 -0700

    add comments

--- contracts/pcv/IPCVDepositAggregator.sol
+++ contracts/pcv/IPCVDepositAggregator.sol
@@ -4,29 +4,45 @@ pragma solidity ^0.8.4;
 import "./IPCVDeposit.sol";
 import "../external/Decimal.sol";
 
-/// @title a PCV Deposit aggregation interface
-/// @author Fei Protocol
+/** 
+ @title a PCV Deposit aggregation interface
+ @author Fei Protocol
+
+ This contract is a single interface for allocating a specific token to multiple PCV Deposits.
+ The aggregator handles new incoming funds and outgoing funds by selecting deposits which are over or under-funded to save for gas and efficiency
+*/
 interface IPCVDepositAggregator is IPCVDeposit {
 
     // ----------- State changing api -----------
+    /// @notice rebalance funds of the underlying deposits to the optimal target percents
     function rebalance() external;
 
     // ----------- Governor only state changing api -----------
-    function addPCVDeposit(address newPCVDeposit) external;
+    /// @notice adds a new PCV Deposit to the set of deposits
+    /// @param weight a relative (i.e. not normalized) weight of this PCV deposit
+    function addPCVDeposit(IPCVDeposit newPCVDeposit, uint256 weight) external;
 
+    /// @notice replaces this contract with a new PCV Deposit Aggregator on the rewardsAssetManager
     function setNewAggregator(IPCVDepositAggregator newAggregator) external;
 
     // ----------- Governor or Guardian only state changing api -----------
-    function removePCVDeposit(address pcvDeposit) external;
+    /// @notice remove a PCV deposit from the set of deposits
+    function removePCVDeposit(IPCVDeposit pcvDeposit) external;
 
     // ----------- Read-only api -----------
+    /// @notice the upstream rewardsAssetManager funding this contract
     function rewardsAssetManager() external returns(address);
 
-    function pcvDeposits() external view returns(IPCVDeposit[] memory);
+    /// @notice the set of PCV deposits and non-normalized weights this contract allocates to
+    function pcvDeposits() external view returns(IPCVDeposit[] memory deposits, uint256[] memory weights);
 
+    /// @notice current percent of PCV held by the input `pcvDeposit` relative to the total managed by aggregator.
+    /// @param depositAmount a hypothetical deposit amount, to be included in the calculation
     function percentHeld(IPCVDeposit pcvDeposit, uint256 depositAmount) external view returns(Decimal.D256 memory);
 
+    /// @notice the normalized target percent of PCV held by `pcvDeposit` relative to aggregator total
     function targetPercentHeld(IPCVDeposit pcvDeposit) external view returns(Decimal.D256 memory);
 
+    /// @notice the raw amount of PCV off of the target percent held by `pcvDeposit`
     function amountFromTarget(IPCVDeposit pcvDeposit) external view returns(int256);
 }
--- contracts/pcv/balancer/IRewardsAssetManager.sol
+++ contracts/pcv/balancer/IRewardsAssetManager.sol
@@ -2,8 +2,17 @@
 
 pragma solidity ^0.8.0;
 
-// interface intended to extend the balancer RewardsAssetManager
-// https://github.com/balancer-labs/balancer-v2-monorepo/blob/389b52f1fc9e468de854810ce9dc3251d2d5b212/pkg/asset-manager-utils/contracts/RewardsAssetManager.sol
+import "../IPCVDepositAggregator.sol";
+
+/**
+@title IRewardsAssetManager
+@author Fei Protocol
+
+interface intended to extend the balancer RewardsAssetManager
+https://github.com/balancer-labs/balancer-v2-monorepo/blob/389b52f1fc9e468de854810ce9dc3251d2d5b212/pkg/asset-manager-utils/contracts/RewardsAssetManager.sol
+
+This contract will essentially pass-through funds to an IPCVDepositAggregator denominated in the same underlying asset
+*/
 interface IRewardsAssetManager {
     // ----------- Governor only state changing api -----------
     function setNewAggregator(address newAggregator) external;
