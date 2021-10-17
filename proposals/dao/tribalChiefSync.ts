9f4932e84bd267d59941133000b546548ad2fb08
Joey Santoro <joseph.santoro@duke.edu>
2021-10-17 14:52:44 -0700

    lint

--- proposals/dao/tribalChiefSync.ts
+++ proposals/dao/tribalChiefSync.ts
@@ -14,51 +14,42 @@ import { increaseTime } from '@test/helpers';
 const blockReward = '71250000000000000000';
 
 const deploy: DeployUpgradeFunc = async (deployAddress: string, addresses: NamedAddresses, logging: boolean) => {
-  
-  const {
-      tribalChief,
-      autoRewardsDistributor,
-      optimisticTimelock
-  } = addresses;
+  const { tribalChief, autoRewardsDistributor, optimisticTimelock } = addresses;
 
   const factory = await ethers.getContractFactory('TribalChiefSync');
-  const tribalChiefSync = await factory.deploy(
-      tribalChief,
-      autoRewardsDistributor,
-      optimisticTimelock
-  );
+  const tribalChiefSync = await factory.deploy(tribalChief, autoRewardsDistributor, optimisticTimelock);
   await tribalChiefSync.deployTransaction.wait();
 
   logging && console.log('tribalChiefSync: ', tribalChiefSync.address);
-  
+
   return {
     tribalChiefSync
   };
 };
 
 const setup: SetupUpgradeFunc = async (addresses, oldContracts, contracts, logging) => {
-    const timelock: OptimisticTimelock = contracts.optimisticTimelock as OptimisticTimelock;
-    await timelock.becomeAdmin();
-    await timelock.grantRole(await timelock.EXECUTOR_ROLE(), addresses.tribalChiefSync);
+  const timelock: OptimisticTimelock = contracts.optimisticTimelock as OptimisticTimelock;
+  await timelock.becomeAdmin();
+  await timelock.grantRole(await timelock.EXECUTOR_ROLE(), addresses.tribalChiefSync);
 
-    await increaseTime(100_000);
+  await increaseTime(100_000);
 };
 
 const teardown: TeardownUpgradeFunc = async (addresses, oldContracts, contracts, logging) => {
-    const tribalChiefSync: TribalChiefSync = contracts.tribalChiefSync as TribalChiefSync;
-    const timelock: OptimisticTimelock = contracts.optimisticTimelock as OptimisticTimelock;
+  const tribalChiefSync: TribalChiefSync = contracts.tribalChiefSync as TribalChiefSync;
+  const timelock: OptimisticTimelock = contracts.optimisticTimelock as OptimisticTimelock;
 
-    const salt = '0xb9fbc6f9768742c095623adb6da7ad118bef79f893487a93d3659b4635ae1cf8';
+  const salt = '0xb9fbc6f9768742c095623adb6da7ad118bef79f893487a93d3659b4635ae1cf8';
 
-    await tribalChiefSync.decreaseRewards(blockReward, salt);
+  await tribalChiefSync.decreaseRewards(blockReward, salt);
 };
 
 const validate: ValidateUpgradeFunc = async (addresses, oldContracts, contracts, logging) => {
-    const tribalChief: TribalChief = contracts.tribalChief as TribalChief;
-    const autoRewardsDistributor: AutoRewardsDistributor = contracts.autoRewardsDistributor as AutoRewardsDistributor;
-    
-    expect(await tribalChief.tribePerBlock()).to.be.bignumber.equal(ethers.BigNumber.from(blockReward));
-    expect((await autoRewardsDistributor.getNewRewardSpeed())[1]).to.be.false;
+  const tribalChief: TribalChief = contracts.tribalChief as TribalChief;
+  const autoRewardsDistributor: AutoRewardsDistributor = contracts.autoRewardsDistributor as AutoRewardsDistributor;
+
+  expect(await tribalChief.tribePerBlock()).to.be.bignumber.equal(ethers.BigNumber.from(blockReward));
+  expect((await autoRewardsDistributor.getNewRewardSpeed())[1]).to.be.false;
 };
 
 export { deploy, setup, teardown, validate };
