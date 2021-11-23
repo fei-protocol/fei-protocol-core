941bb5837461d0284b185d0393e6d09e9443ef26
Caleb <ditchfieldcaleb@gmail.com>
2021-11-22 23:10:22 -0800

    proposal steps

--- proposals/dao/transferLiquidity.ts
+++ proposals/dao/transferLiquidity.ts
@@ -6,7 +6,9 @@ import {
   SetupUpgradeFunc,
   TeardownUpgradeFunc,
   ValidateUpgradeFunc
-} from '../../types/types';
+} from '@custom-types/types';
+import { Fei, LinearTokenTimelock, LinearTokenTimelock__factory, Tribe } from '@custom-types/contracts';
+import { getImpersonatedSigner } from '@test/helpers';
 
 /*
 
@@ -24,10 +26,14 @@ Steps:
   3 - call unlockLiquidity on old time lock
   4 - call releaseMax to helper contract
   5 - call doLiquidityTransfer on helper contract
+  6 - set pending beneficary back to guardian for dust collection
   6 - update collateralization oracle?
 */
 
 const fipNumber = '9001'; // Change me!
+const OLD_TIMELOCK_ADDRESS = '0x7D809969f6A04777F0A87FF94B57E56078E5fE0F';
+const GUARDIAN_ADDRESS = '0xB8f482539F2d3Ae2C9ea6076894df36D1f632775';
+const FEI_TRIBE_LP_ADDRESS = '0x9928e4046d7c6513326cCeA028cD3e7a91c7590A';
 
 // Do any deployments
 // This should exclusively include new contract deployments
@@ -53,19 +59,76 @@ const deploy: DeployUpgradeFunc = async (deployAddress: string, addresses: Named
 // This could include setting up Hardhat to impersonate accounts,
 // ensuring contracts have a specific state, etc.
 const setup: SetupUpgradeFunc = async (addresses, oldContracts, contracts, logging) => {
-  console.log(`No actions to complete in setup for fip${fipNumber}`);
+  if (!addresses.timelock) throw new Error('Timelock address was not set!');
+
+  // Change beneficiary of existing timelock from guardian multisig to timelock
+  // Old timelock @ 0x7D809969f6A04777F0A87FF94B57E56078E5fE0F
+  const oldTImelock = await ethers.getContractAt(
+    'LinearTokenTimelock',
+    OLD_TIMELOCK_ADDRESS,
+    await getImpersonatedSigner(GUARDIAN_ADDRESS)
+  );
+  await oldTImelock.setPendingBeneficiary(addresses.timelock);
+
+  // Make sure the pending beneficiary is set to the timelock
+  const pendingBeneficiary = await oldTImelock.pendingBeneficiary();
+  expect(pendingBeneficiary).to.equal(addresses.timelock);
 };
 
 // Tears down any changes made in setup() that need to be
 // cleaned up before doing any validation checks.
 const teardown: TeardownUpgradeFunc = async (addresses, oldContracts, contracts, logging) => {
-  console.log(`No actions to complete in teardown for fip${fipNumber}`);
+  // Change beneficiary of existing timelock from timelock back to guardian multisig
+  const oldTImelock = await ethers.getContractAt(
+    'LinearTokenTimelock',
+    OLD_TIMELOCK_ADDRESS,
+    await getImpersonatedSigner(GUARDIAN_ADDRESS)
+  );
+  await oldTImelock.setPendingBeneficiary(addresses.timelock);
 };
 
 // Run any validations required on the fip using mocha or console logging
 // IE check balances, check state of contracts, etc.
 const validate: ValidateUpgradeFunc = async (addresses, oldContracts, contracts, logging) => {
-  console.log(`No actions to complete in validate for fip${fipNumber}`);
+  const oldTImelock = await ethers.getContractAt(
+    'LinearTokenTimelock',
+    OLD_TIMELOCK_ADDRESS,
+    await getImpersonatedSigner(GUARDIAN_ADDRESS)
+  );
+
+  // Beneficiary of old timelock should be the guardian multisig
+  expect(await oldTImelock.beneficiary()).to.equal(GUARDIAN_ADDRESS);
+
+  // Pending beneficiary of old timelock should be 0x0
+  expect(await oldTImelock.pendingBeneficiary()).to.equal(ethers.constants.AddressZero);
+
+  // The old timelock shouldn't have any LP left
+  const FEI_TRIBE_LP = await ethers.getContractAt('ERC20', FEI_TRIBE_LP_ADDRESS);
+  expect(await FEI_TRIBE_LP.balanceOf(OLD_TIMELOCK_ADDRESS)).to.equal(ethers.constants.Zero);
+
+  // The old timelock shouldn't have any trip or fei left
+  expect(await (contracts.tribe as Tribe).balanceOf(OLD_TIMELOCK_ADDRESS)).to.equal(ethers.constants.Zero);
+  expect(await (contracts.fei as Fei).balanceOf(OLD_TIMELOCK_ADDRESS)).to.equal(ethers.constants.Zero);
+
+  // The new timelocks should have the tribe and the fei
+  expect(await (contracts.tribe as Tribe).balanceOf(addresses.feiGovernanceTimelock)).to.not.equal(
+    ethers.constants.Zero
+  );
+  expect(await (contracts.fei as Fei).balanceOf(addresses.feiGovernanceTimelock)).to.not.equal(ethers.constants.Zero);
+  expect(await (contracts.tribe as Tribe).balanceOf(addresses.tribeGovernanceTimelock)).to.be.gt(
+    ethers.utils.parseEther('50000000')
+  ); // 50 million tribe
+  expect(await (contracts.fei as Fei).balanceOf(addresses.tribeGovernanceTimelock)).to.be.gt(
+    ethers.utils.parseEther('50000000')
+  ); // 50 million fei
+
+  // The beneficiary of the new timelocks should be the guardian multisig
+  expect(await contracts.tribeGovernanceTimelock.beneficiary()).to.equal(GUARDIAN_ADDRESS);
+  expect(await contracts.feiGovernanceTimelock.beneficiary()).to.equal(GUARDIAN_ADDRESS);
+
+  // Pending beneficiaries of the new timelocks should be 0x0
+  expect(await contracts.tribeGovernanceTimelock.pendingBeneficiary()).to.equal(ethers.constants.AddressZero);
+  expect(await contracts.feiGovernanceTimelock.pendingBeneficiary()).to.equal(ethers.constants.AddressZero);
 };
 
 export { deploy, setup, teardown, validate };
