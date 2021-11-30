d6e5879ea6a5ba4302faf357f0d7bf3c41a8662d
Elliot <elliotfriedman3@gmail.com>
2021-11-30 13:41:47 -0800

    add syn address

--- contract-addresses/mainnetAddresses.ts
+++ contract-addresses/mainnetAddresses.ts
@@ -445,6 +445,10 @@ const MainnetAddresses = {
     artifactName: 'StakingTokenWrapper',
     address: '0x90B336dFF819b9e4b3D9A32cabdcAB0E92836065'
   },
+  stakingTokenWrapperSYNLaaS: {
+    artifactName: 'StakingTokenWrapper',
+    address: '0x5Db85e395735Bb42eEB720Fe2EE69627d246e300'
+  },
   stAAVE: { artifactName: 'IERC20', address: '0x4da27a545c0c5b758a6ba100e3a049001de870f5' },
   steth: { artifactName: 'IERC20', address: '0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84' },
   sushiswapDpiFei: { artifactName: 'IUniswapV2Pair', address: '0x8775aE5e83BC5D926b6277579c2B0d40c7D9b528' },
