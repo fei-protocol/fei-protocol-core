fb4f98f66134cc1ad4f8b2aeaa21c2aea8c85684
Joey Santoro <joseph.santoro@duke.edu>
2021-12-04 16:33:00 -0800

    description

--- proposals/description/permanently_revoke_burner.ts
+++ proposals/description/permanently_revoke_burner.ts
@@ -11,7 +11,13 @@ const permanently_revoke_burner: ProposalDescription = {
       description: 'Set restricted permissions to core for Fei contract'
     }
   ],
-  description: 'Permanently Revoke Burner'
+  description: `
+  Replace the core reference in the FEI token to a “Restricted Permissions” which only allows for minting and pausing. 
+  This would permanently lock the contract’s ability to burn from any address. It preserves the ability for a user or contract to burn its own FEI.
+
+  Code: https://github.com/fei-protocol/fei-protocol-core/blob/feat/v2/base/contracts/core/RestrictedPermissions.sol
+  Discussion: https://tribe.fei.money/t/fip-54-permanently-deprecate-burner/3743 
+`
 };
 
 export default permanently_revoke_burner;
