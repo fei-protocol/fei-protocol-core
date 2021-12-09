1673e171d44881670df81921807dd3e96afdabbb
Elliot <elliotfriedman3@gmail.com>
2021-12-08 17:43:56 -0800

    remove .only from integration tests

--- test/integration/tests/psm.ts
+++ test/integration/tests/psm.ts
@@ -18,7 +18,7 @@ before(async () => {
   await resetFork();
 });
 
-describe.only('e2e-peg-stability-module', function () {
+describe('e2e-peg-stability-module', function () {
   const impersonatedSigners: { [key: string]: Signer } = {};
   let contracts: NamedContracts;
   let deployAddress: string;
