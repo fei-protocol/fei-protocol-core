e329f8cfc80c5ff2a8ba342a3f92f9d124eb5478
Erwan Beauvois <erwan@allmyt.eu>
2021-12-13 20:04:26 +0100

    empty dependencies.ts

--- contract-addresses/dependencies.ts
+++ contract-addresses/dependencies.ts
@@ -1,20 +1,5 @@
 import { DependencyMap } from '@custom-types/types';
 
-const dependencies: DependencyMap = {
-  d3poolCurvePCVDeposit: {
-    fips: {
-      fip_53: true
-    },
-    contractDependencies: [],
-    externalDependencies: []
-  },
-  d3poolConvexPCVDeposit: {
-    fips: {
-      fip_53: true
-    },
-    contractDependencies: [],
-    externalDependencies: []
-  }
-};
+const dependencies: DependencyMap = {};
 
 export default dependencies;
