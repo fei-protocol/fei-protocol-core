9bb4deff3cbb88236ea9f52201dc729114337ee2
Joey Santoro <joseph.santoro@duke.edu>
2022-01-09 14:04:56 -0800

    proposals config

--- test/integration/proposals_config.ts
+++ test/integration/proposals_config.ts
@@ -2,7 +2,6 @@ import { ProposalCategory, ProposalsConfigMap } from '@custom-types/types';
 
 // import fip_xx_proposal from '@proposals/description/fip_xx';
 
-import fip_60 from '@proposals/description/fip_60';
 import fip_60b from '@proposals/description/fip_60b';
 import fip_62 from '@proposals/description/fip_62';
 
@@ -15,25 +14,6 @@ const proposals: ProposalsConfigMap = {
         proposal: fip_xx_proposal // full proposal file, imported from '@proposals/description/fip_xx.ts'
     }
     */
-  fip_60: {
-    deploy: false,
-    proposalId: undefined,
-    affectedContractSignoff: [
-      'rariPool8Comptroller',
-      'rariPool8MasterOracle',
-      'd3StakingTokenWrapper',
-      'tribalChief',
-      'fei3CrvStakingtokenWrapper',
-      'd3AutoRewardsDistributor',
-      'fei3CrvAutoRewardsDistributor',
-      'rewardsDistributorAdmin',
-      'fuseGuardian'
-    ],
-    deprecatedContractSignoff: [],
-    category: ProposalCategory.OA,
-    totalValue: 0,
-    proposal: fip_60
-  },
   fip_60b: {
     deploy: true,
     proposalId: undefined,
@@ -52,29 +32,29 @@ const proposals: ProposalsConfigMap = {
     category: ProposalCategory.OA,
     totalValue: 0,
     proposal: fip_60b
+  },
+  fip_62: {
+    deploy: true,
+    proposalId: undefined,
+    affectedContractSignoff: [
+      'ethPSM',
+      'PSMRouter',
+      'aaveEthPCVDripController',
+      'collateralizationOracle',
+      'bondingCurve',
+      'ethReserveStabilizer',
+      'ethReserveStabilizerWrapper'
+    ],
+    deprecatedContractSignoff: [
+      'compoundEthPCVDripController',
+      'bondingCurve',
+      'ethReserveStabilizer',
+      'ethReserveStabilizerWrapper'
+    ],
+    category: ProposalCategory.DAO,
+    totalValue: 0,
+    proposal: fip_62
   }
-  // fip_62: {
-  //   deploy: true,
-  //   proposalId: undefined,
-  //   affectedContractSignoff: [
-  //     'ethPSM',
-  //     'PSMRouter',
-  //     'aaveEthPCVDripController',
-  //     'collateralizationOracle',
-  //     'bondingCurve',
-  //     'ethReserveStabilizer',
-  //     'ethReserveStabilizerWrapper'
-  //   ],
-  //   deprecatedContractSignoff: [
-  //     'compoundEthPCVDripController',
-  //     'bondingCurve',
-  //     'ethReserveStabilizer',
-  //     'ethReserveStabilizerWrapper'
-  //   ],
-  //   category: ProposalCategory.DAO,
-  //   totalValue: 0,
-  //   proposal: fip_62
-  // }
 };
 
 export default proposals;
