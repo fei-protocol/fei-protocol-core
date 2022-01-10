4ce2698e6a86365d836cf75afc8d0a6cc7021458
Elliot <elliotfriedman3@gmail.com>
2022-01-10 09:29:29 -0800

    remove fip-60 from proposal config

--- test/integration/proposals_config.ts
+++ test/integration/proposals_config.ts
@@ -2,7 +2,6 @@ import { ProposalCategory, ProposalsConfigMap } from '@custom-types/types';
 
 // import fip_xx_proposal from '@proposals/description/fip_xx';
 
-import fip_60 from '@proposals/description/fip_60';
 import fip_62 from '@proposals/description/fip_62';
 
 const proposals: ProposalsConfigMap = {
@@ -14,25 +13,6 @@ const proposals: ProposalsConfigMap = {
         proposal: fip_xx_proposal // full proposal file, imported from '@proposals/description/fip_xx.ts'
     }
     */
-  // fip_60: {
-  //   deploy: false,
-  //   proposalId: undefined,
-  //   affectedContractSignoff: [
-  //     'rariPool8Comptroller',
-  //     'rariPool8MasterOracle',
-  //     'd3StakingTokenWrapper',
-  //     'tribalChief',
-  //     'fei3CrvStakingtokenWrapper',
-  //     'd3AutoRewardsDistributor',
-  //     'fei3CrvAutoRewardsDistributor',
-  //     'rewardsDistributorAdmin',
-  //     'fuseGuardian'
-  //   ],
-  //   deprecatedContractSignoff: [],
-  //   category: ProposalCategory.OA,
-  //   totalValue: 0,
-  //   proposal: fip_60
-  // },
   fip_62: {
     deploy: false,
     proposalId: '85955812255891965249900586182423874595375644650518174119671869653470301469586',
