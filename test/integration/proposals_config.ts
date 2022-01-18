218ac205c808080027425f407ec36eff84473927
Joey Santoro <joseph.santoro@duke.edu>
2022-01-17 17:03:33 -0800

    cleanup proposals_config

--- test/integration/proposals_config.ts
+++ test/integration/proposals_config.ts
@@ -2,11 +2,6 @@ import { ProposalCategory, ProposalsConfigMap } from '@custom-types/types';
 
 // import fip_xx_proposal from '@proposals/description/fip_xx';
 
-import fip_60b from '@proposals/description/old/fip_60b';
-import fip_67 from '@proposals/description/fip_67';
-import fip_64 from '@proposals/description/fip_64';
-import fip_redeem from '@proposals/description/fip_redeem';
-
 const proposals: ProposalsConfigMap = {
   /*
     fip_xx : {
@@ -16,65 +11,6 @@ const proposals: ProposalsConfigMap = {
         proposal: fip_xx_proposal // full proposal file, imported from '@proposals/description/fip_xx.ts'
     }
     */
-  fip_redeem: {
-    deploy: false,
-    proposalId: undefined,
-    affectedContractSignoff: ['reptbRedeemer', 'fei', 'pegExchanger'],
-    deprecatedContractSignoff: [],
-    category: ProposalCategory.DAO,
-    totalValue: 0,
-    proposal: fip_redeem
-  },
-  fip_60b: {
-    deploy: false,
-    proposalId: undefined,
-    affectedContractSignoff: [
-      'rewardsDistributorAdmin',
-      'fuseAdmin',
-      'rariPool8Comptroller',
-      'rariPool8MasterOracle',
-      'gUniFuseOracle',
-      'rariPool8EthIrm',
-      'rariPool8CTokenImpl',
-      'fuseGuardian',
-      'tribalChief',
-      'feiDaiStakingTokenWrapper',
-      'feiUsdcStakingTokenWrapper',
-      'feiDaiAutoRewardsDistributor',
-      'feiUsdcAutoRewardsDistributor'
-    ],
-    deprecatedContractSignoff: [],
-    category: ProposalCategory.OA,
-    totalValue: 0,
-    proposal: fip_60b
-  },
-  fip_67: {
-    deploy: false,
-    proposalId: undefined,
-    affectedContractSignoff: [
-      'lusdPSM',
-      'lusdPCVDripController',
-      'lusdPSMFeiSkimmer',
-      'collateralizationOracle',
-      'core',
-      'pcvGuardian',
-      'rariPool146EthPCVDeposit',
-      'compoundEthPCVDeposit'
-    ],
-    deprecatedContractSignoff: [],
-    category: ProposalCategory.DAO,
-    totalValue: 0,
-    proposal: fip_67
-  },
-  fip_64: {
-    deploy: false,
-    proposalId: undefined,
-    affectedContractSignoff: ['fuseAdmin', 'rariPool8EthIrm', 'rariPool8CTokenImpl', 'fuseGuardian'],
-    deprecatedContractSignoff: [],
-    category: ProposalCategory.OA,
-    totalValue: 0,
-    proposal: fip_64
-  }
 };
 
 export default proposals;
