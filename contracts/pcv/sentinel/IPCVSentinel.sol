d571bdac85d7dc5931eb012c401c782d2ff83bc4
Caleb <ditchfieldcaleb@gmail.com>
2022-01-26 22:27:06 -0800

    simplify

--- contracts/pcv/sentinel/IPCVSentinel.sol
+++ contracts/pcv/sentinel/IPCVSentinel.sol
@@ -6,23 +6,17 @@ pragma solidity ^0.8.4;
 /// @dev any implementation of this contract should be granted the roles of Guardian and PCVController in order to work correctly
 interface IPCVSentinel {
     // ---------- Events ----------
-    event ContractProtected(address indexed protectedContract, address indexed guard);
-    event ContractGuardAdded(address indexed protectedContract, address indexed guard);
-    event ContractGuardRemoved(address indexed protectedContract, address indexed guard);
-
-    // ---------- Read-Only API ----------
-    function getGuardedContracts() external view returns (address[] memory guarded);
-    function getAllGuards() external view returns (address[] memory allGuards);
-    function getContractsAndGuards() external view returns (address[][] memory contractsAndGuards);
+    event Protected(address indexed guard);
+    event GuardAdded(address indexed guard);
+    event GuardRemoved(address indexed guard);
 
     // ---------- Governor-Only State-Changing API ----------
-    function knight(address guard, address guardedContract) external;
+    function knight(address guard) external;
 
     // ---------- Governor-Or-Admin-Or-Guardian-Only State-Changing API ----------
     function slay(address traitor) external;
 
     // ---------- Public State-Changing API ----------
-    function protec(address guardedContract) external returns (bool activated);
-    function activateGuard(address guardAddress) external returns (bool);
-    function activateAllGuards() external payable returns (bool activated, address[] memory activatedGuards);
+    function protec(address guard) external;
+    function protecMany(address[] memory guards) external;
 }
--- contracts/pcv/sentinel/PCVSentinel.sol
+++ contracts/pcv/sentinel/PCVSentinel.sol
@@ -12,214 +12,110 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 /**
  * @title PCV Sentinel
  * @dev the PCV Sentinel should be granted the role Guardian
- * @notice The PCV Sentinel is a general-purpose protector with extremely flexible powers.
- * On its own, it is useless; it must be paired with "guards". Guards are deployed smart contracts
- * that each know how to protect a specific contract (typically a pcv deposit), and know what actions
- * to take in order to protect their contract. Each protected contract can have many guards; each guard
- * is assigned to a single smart contract. The PCV Sentinel delegates (via delegatecall) to each guard
- * to perform actions with the PCVSentinel's powers (guardian and pcv controller). The DAO
- * can choose to add or remove guards to the contract. Each guard should be THOUROUGLY reviewed.
- * Anyone can activate guards, allowing the Sentinel to act instantly in time of need.
+ * @notice The PCV Sentinel is a automated extension of the Guardian.
+ * The Guardian can add Guards to the PCV Sentinel. Guards run checks
+ * and provide addresses and calldata for the Sentinel to run, if needed.
  */
-contract PCVSentinel is IPCVSentinel, CoreRef {
+contract PCVSentinel is IPCVSentinel, CoreRef, ReentrancyGuard {
     using EnumerableSet for EnumerableSet.AddressSet;
 
-    // Each contract can have many guards
-    // But each guard can guard only one contract
-    EnumerableSet.AddressSet private guardedContracts;
+    // The set of all guards
     EnumerableSet.AddressSet private guards;
-
-    // This mapping gives us the contract to which the supplied guard applies to
-    mapping(address => address) public guardToContract;
-
-    // This mapping gives us all of the guards of the supplied contract
-    mapping(address => EnumerableSet.AddressSet) private contractToGuards;
 
     constructor(
         address _core
-    ) CoreRef(_core) {
+    ) CoreRef(_core) ReentrancyGuard() {
         _setContractAdminRole(keccak256("PCV_SENTINEL_ADMIN_ROLE"));
     }
 
     // ---------- Read-Only API ----------
 
     /**
-     * @notice Get all contracts that are guarded
-     * @return guarded an array of guarded contracts
+     * @notice returns the list of all guards that pass their check
+     * @dev you *can* use this on-chain if you want, but this is made
+     * to be called off-chain to pass into protecManys and not hyper-efficient
      */
-    function getGuardedContracts() external override view returns (address[] memory guarded) {
-        guarded = new address[](guardedContracts.length());
-        for (uint i = 0; i < guardedContracts.length(); i++) {
-            guarded[i] = guardedContracts.at(i);
-        }
+    function checkAll() public view returns (address[] memory) {
+        uint256 numGuardsCanProtec;
 
-        return guarded;
-    }
-
-    /**
-     * @notice Get all guards
-     * @return allGuards an array of all guards
-     */
-    function getAllGuards() external override view returns (address[] memory allGuards) {
-        allGuards = new address[](guards.length());
-        for (uint i = 0; i < guards.length(); i++) {
-            allGuards[i] = guards.at(i);
-        }
-
-        return allGuards;
-    }
-
-    /**
-     * @notice Get all contracts and their guards
-     * @return contractsAndGuards a 2-dimensional array of contracts and their guards
-     */
-    function getContractsAndGuards() external override view returns (address[][] memory contractsAndGuards) {
-        contractsAndGuards = new address[][](guardedContracts.length());
-        for (uint i = 0; i < guardedContracts.length(); i++) {
-            address guardedContract = guardedContracts.at(i);
-            uint256 numGuards = contractToGuards[guardedContract].length();
-
-            address[] memory guardsForContract = new address[](numGuards);
-
-            for (uint j = 0; j < numGuards; j++) {
-                guardsForContract[j] = contractToGuards[guardedContract].at(j);
+        for (uint256 i=0; i<guards.length(); i++) {
+            if(IGuard(guards.at(i)).check()) {
+                numGuardsCanProtec++;
             }
-
-            contractsAndGuards[i] = guardsForContract;
         }
 
-        return contractsAndGuards;
-    }
+        address[] memory guardsWhoCanProtec = new address[](numGuardsCanProtec);
 
-    // ---------- Governor-Only State-Changing API ----------
+        for (uint256 i=0; i<guards.length(); i++) {
+            if(IGuard(guards.at(i)).check()) {
+                numGuardsCanProtec--;
+                guardsWhoCanProtec[numGuardsCanProtec] = guards.at(i);
+            }
+        }
 
-    /**
-     * @notice adds a guard and its associated contract
-     * @dev Guards are given the full power of the Guardian and PCVController roles and should be added carefully.
-     *      They are activated via delegateCall and will take arbitrary actions on behalf of the Sentinel.
-     *      Tread carefully; with great power comes great responsibility.
-     * @param knight the guard-contract to add
-     * @param guardedContract the contract that the guard protects
-     */
-    function knight(address guard, address guardedContract) external override onlyGovernor {
-        // .add on addressSet is a no-op if the address is already in the set
-        guardedContracts.add(guardedContract);
-        guards.add(guard);
-
-        // Assign the newly appointed knight to his fief (contract)
-        guardToContract[guard] = guardedContract;
-
-        // Let the fief know that it has a new guard
-        contractToGuards[guardedContract].add(guard);
-
-        // Inform the kingdom of this glorious news
-        emit ContractGuardAdded(guardedContract, guard);
+        return guardsWhoCanProtec;
     }
 
     // ---------- Governor-or-Admin-Or-Guardian-Only State-Changing API ----------
 
     /**
+     * @notice adds a guard contract to the PCV Sentinel
+     * @param guard the guard-contract to add
+     */
+    function knight(address guard) 
+        external 
+        override 
+        onlyGuardianOrGovernor
+        nonReentrant
+    {
+        guards.add(guard);
+
+        // Inform the kingdom of this glorious news
+        emit GuardAdded(guard);
+    }
+
+    /**
      * @notice removes a guard
      * @param traitor the guard-contract to remove
      */
-    function slay(address traitor) external override isGovernorOrGuardianOrAdmin {
-        /// Find out which address the traitor was guarding
-        address guardedContract = guardToContract[traitor];
-
-        // Unassign the guard from the global registry
+    function slay(address traitor) 
+        external 
+        override 
+        onlyGuardianOrGovernor
+        nonReentrant
+    {
         guards.remove(traitor);
 
-        // Unassign the guard from his contract
-        delete(guardToContract[traitor]);
-        
-        // Remove the guard from the list of guards that his contract has
-        contractToGuards[guardedContract].remove(traitor);
-
         // Inform the kingdom of this sudden and inevitable betrayal
-        emit ContractGuardRemoved(guardedContract, traitor);
+        emit GuardRemoved(traitor);
     }
 
-    // Public State-Changing API
+    // ---------- Public State-Changing API ----------
 
-    /**
-     * @notice Activate all guards for a given contract
-     * @param guardedContract the contract for which to activate its guards, if any
-     * @return activated true if any guards took action
-     */
-    function protec(address guardedContract) external override returns (bool activated) {
-        require(guardedContracts.contains(guardedContract));
+    function protec(address guard) 
+        public 
+        override
+        nonReentrant
+    {
+        require(guards.contains(guard), "Guard does not exist.");
+        (bool needsProtec, address[] targets, bytes[] datas) = IGuard(guard).check();
 
-        for (uint i = 0; i < contractToGuards[guardedContract].length(); i++) {
-            bool thisGuardActivated = activateGuard(contractToGuards[guardedContract].at(i));
-            if (thisGuardActivated) {
-                emit ContractProtected(guardedContract, contractToGuards[guardedContract].at(i));
-            }
-            activated = activated || thisGuardActivated;
-        }
-
-        return activated;
-    }
-
-    function activateGuards(address[] calldata guardAddresses) external returns (bool) {
-        for (uint i = 0; i < guardAddresses.length; i++) {
-            activateGuard(guardAddresses[i]);
-        }
-    }
-
-
-    /**
-     * @notice Activate an individual guard by calling it directly
-     * @param guardAddress the address of the guard to activate
-     * @return true if the guard took any action
-     */
-    function activateGuard(address guardAddress) public override nonReentrant returns (bool) {
-        require(guards.contains(guardAddress));
-        address contractAddress = guardToContract[guardAddress];
-
-        if (IGuard(guardAddress).check()) {
-            (address[] memory targets, bytes[] memory calldatas, bool allowReverts) = IGuard(guardAddress).getProtecActions();
-            require(targets.length == calldatas.length, "Targets and calldatas must be the same length");
-            for(uint i=0; i<targets.length; i++) {
-                require(targets[i] != address(this), "Stahp");
-                (bool success,) = targets[i].call(calldatas[i]);
-                if (!success && !allowReverts) revert("Guard action failed and reverts not allowed.");
+        if (needsProtec) {
+            for(uint256 i=0; i<targets.length; i++) {
+                targets[i].call(datas[i]); // Unopinionated about reverts
             }
 
-            emit ContractProtected(contractAddress, guardAddress);
-            return true;
+            emit Protected(guard);
         }
-
-        return false;
     }
 
-    /**
-     * @notice Activate all guards
-     * @return activated true if any guards took action
-     * @return activatedGuards the addresses of guards that took action
-     * @dev use this function with ethers.js staticCall() to get a list of activate-able guards
-     */
-    function activateAllGuards() external payable override returns (bool activated, address[] memory activatedGuards) {
-        activatedGuards = new address[](guards.length());
-        uint numGuardsActivated = 0;
-
-        for (uint i = 0; i < guards.length(); i++) {
-            bool thisGuardActivated = activateGuard(guards.at(i));
-            if (thisGuardActivated) {
-                activatedGuards[numGuardsActivated] = guards.at(i);
-                numGuardsActivated++;
-                emit ContractProtected(guardToContract[guards.at(i)], guards.at(i));
-            }
-            activated = activated || thisGuardActivated;
+    function protecMany(address[] calldata whichGuards) 
+        public 
+        override
+        nonReentrant
+    {
+        for(uint256 i=0; i<whichGuards.length; i++) {
+            protec(whichGuards[i]);
         }
-
-        address[] memory activatedGuardsTruncated = new address[](numGuardsActivated);   
-
-        // Copy the activated guards over to the truncated-size array
-        // since you cant resize arrays and dynamic memory arrays dont exist
-        for(uint i = 0; i < numGuardsActivated; i++) {
-            activatedGuardsTruncated[i] = activatedGuards[i];
-        }
-
-        return (activated, activatedGuardsTruncated);
     }
 }
