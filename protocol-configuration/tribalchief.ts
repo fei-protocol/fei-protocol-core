27294ab86d3b00c83b42a7516dadb650a3cc10ef
Erwan Beauvois <erwan@allmyt.eu>
2022-02-07 15:23:24 -0800

    Update tribalchief config to include FIP-77 changes

--- protocol-configuration/tribalchief.ts
+++ protocol-configuration/tribalchief.ts
@@ -1,6 +1,6 @@
 export default {
   feiTribePair: { allocPoint: 0, unlocked: false }, // Uniswap-v2 FEI/TRIBE LP
-  curve3Metapool: { allocPoint: 1000, unlocked: false }, // Curve 3crv-FEI metapool LP
+  curve3Metapool: { allocPoint: 0, unlocked: false }, // Curve 3crv-FEI metapool LP
   gUniFeiDaiLP: { allocPoint: 0, unlocked: true }, // G-UNI DAI/FEI 0.05% fee tier
   stakingTokenWrapperRari: { allocPoint: 1000, unlocked: false }, // FeiRari: TRIBE
   stakingTokenWrapperGROLaaS: { allocPoint: 50, unlocked: false }, // LaaS: GRO
@@ -13,7 +13,7 @@ export default {
   stakingTokenWrapperPoolTogetherLaaS: { allocPoint: 13, unlocked: false }, // LaaS: PoolTogether
   stakingTokenWrapperBribeD3pool: { allocPoint: 250, unlocked: false }, // Votium bribes: d3pool
   d3StakingTokenWrapper: { allocPoint: 250, unlocked: false }, // FeiRari: d3pool LP
-  fei3CrvStakingtokenWrapper: { allocPoint: 250, unlocked: false }, // FeiRari: 3crv-FEI metapool LP
+  fei3CrvStakingtokenWrapper: { allocPoint: 1000, unlocked: false }, // FeiRari: 3crv-FEI metapool LP
   feiDaiStakingTokenWrapper: { allocPoint: 100, unlocked: false }, // FeiRari: G-UNI DAI/FEI 0.05% fee tier
   feiUsdcStakingTokenWrapper: { allocPoint: 500, unlocked: false }, // FeiRari: G-UNI USDC/FEI 0.01% fee tier
   stakingTokenWrapperBribe3Crvpool: { allocPoint: 250, unlocked: false } // Votium bribes: 3crv-FEI metapool
