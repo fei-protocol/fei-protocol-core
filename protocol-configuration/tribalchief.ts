200d35aa8826adec8b0f5e5778424894b694401d
Erwan Beauvois <erwan@allmyt.eu>
2022-02-07 19:51:07 -0800

    Add type for the TribalChief config

--- protocol-configuration/tribalchief.ts
+++ protocol-configuration/tribalchief.ts
@@ -1,4 +1,6 @@
-export default {
+import { TribalChiefConfig } from '../types/types';
+
+const config: TribalChiefConfig = {
   feiTribePair: { allocPoint: 0, unlocked: false }, // Uniswap-v2 FEI/TRIBE LP
   curve3Metapool: { allocPoint: 0, unlocked: false }, // Curve 3crv-FEI metapool LP
   gUniFeiDaiLP: { allocPoint: 0, unlocked: true }, // G-UNI DAI/FEI 0.05% fee tier
@@ -18,3 +20,5 @@ export default {
   feiUsdcStakingTokenWrapper: { allocPoint: 500, unlocked: false }, // FeiRari: G-UNI USDC/FEI 0.01% fee tier
   stakingTokenWrapperBribe3Crvpool: { allocPoint: 250, unlocked: false } // Votium bribes: 3crv-FEI metapool
 };
+
+export default config;
