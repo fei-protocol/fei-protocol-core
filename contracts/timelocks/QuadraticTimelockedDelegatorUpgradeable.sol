fab11afb8859ff89406b06662463653303202a30
davidlucid <davidlucidsl@gmail.com>
2022-02-11 14:53:30 -0800

    QuadraticTimelockedDelegatorUpgradeable

---
+++ contracts/timelocks/QuadraticTimelockedDelegatorUpgradeable.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./QuadraticTokenTimelockUpgradeable.sol";
+
+interface IVotingToken is IERC20 {
+    function delegate(address delegatee) external;
+}
+
+/// @title a timelock for tokens allowing for bulk delegation
+/// @author Fei Protocol
+/// @notice allows the timelocked tokens to be delegated by the beneficiary while locked
+contract QuadraticTimelockedDelegatorUpgradeable is QuadraticTokenTimelockUpgradeable {
+    /// @notice accept beneficiary role over timelocked TRIBE
+    function acceptBeneficiary() public override {
+        _setBeneficiary(msg.sender);
+    }
+
+    /// @notice delegate all held TRIBE to the `to` address
+    function delegate(address to) public onlyBeneficiary {
+        IVotingToken(address(lockedToken)).delegate(to);
+    }
+}
