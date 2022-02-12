ab43f3962488e12dd6ccd4251383af3bf041da0c
thomas-waite <thomas.waite4@gmail.com>
2022-02-11 16:14:09 -0800

    build: add backup oracle deploy params

--- protocol-configuration/backupOracleConfig.ts
+++ protocol-configuration/backupOracleConfig.ts
@@ -1,21 +1,23 @@
-export const backupOracleConfig: BackupOracleConfig = {
-  daiUsdc: {
-    secondsAgo: 1800, // 30 minutes (60*10*3)
-    uniswapPool: '0x5777d92f208679DB4b9778590Fa3CAB3aC9e2168' // TODO: Check
-  },
-  ethUsdc: {
-    secondsAgo: 600, // 10 minutes (60 * 10)
-    uniswapPool: '0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8' // TODO: Check + may need inverting
-  }
+export const daiUsdcBackupOracleConfig: BackupOracleConfig = {
+  twapPeriod: 1800, // 30 minutes
+  minTwapPeriod: 60, // 1 minute
+  maxTwapPeriod: 86400, // 1 day
+  minPoolLiquidity: 50e6, // 50m
+  uniswapPool: '0x5777d92f208679DB4b9778590Fa3CAB3aC9e2168' // TODO: Check
+};
+
+export const ethUsdcBackupOracleConfig: BackupOracleConfig = {
+  twapPeriod: 1800, // 30 minutes
+  minTwapPeriod: 1800, // 30 minutes
+  maxTwapPeriod: 172800, // 2 days
+  minPoolLiquidity: 50e6, // 50m
+  uniswapPool: '0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8' // TODO: Check + may need inverting
 };
 
 export type BackupOracleConfig = {
-  daiUsdc: {
-    secondsAgo: number;
-    uniswapPool: string;
-  };
-  ethUsdc: {
-    secondsAgo: number;
-    uniswapPool: string;
-  };
+  twapPeriod: number;
+  minTwapPeriod: number;
+  maxTwapPeriod: number;
+  minPoolLiquidity: number;
+  uniswapPool: string;
 };
