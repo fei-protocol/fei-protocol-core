547199772902b6e6507a629cbc7ce642257bc486
Erwan Beauvois <erwan@allmyt.eu>
2022-02-14 18:42:18 +0100

    Remove passed proposals from proposal config

--- test/integration/proposals_config.ts
+++ test/integration/proposals_config.ts
@@ -2,8 +2,6 @@ import { ProposalCategory, ProposalsConfigMap } from '@custom-types/types';
 
 // import fip_xx_proposal from '@proposals/description/fip_xx';
 
-import fip_76b from '@proposals/description/fip_76b';
-import fip_77 from '@proposals/description/fip_77';
 import fip_78a from '@proposals/description/fip_78a';
 
 const proposals: ProposalsConfigMap = {
@@ -15,44 +13,6 @@ const proposals: ProposalsConfigMap = {
         proposal: fip_xx_proposal // full proposal file, imported from '@proposals/description/fip_xx.ts'
     }
     */
-  fip_76b: {
-    deploy: false,
-    proposalId: undefined,
-    affectedContractSignoff: [
-      'delayedPCVMoverWethUniToBal',
-      'balancerDepositFeiWeth',
-      'aaveEthPCVDeposit',
-      'uniswapPCVDeposit',
-      'compoundDaiPCVDeposit',
-      'bammDeposit',
-      'ratioPCVControllerV2',
-      'feiDAOTimelock',
-      'collateralizationOracle',
-      'rariPool8DaiPCVDeposit',
-      'rariPool8LusdPCVDeposit',
-      'votiumBriber3Crvpool'
-    ],
-    deprecatedContractSignoff: [
-      'dpiUniswapPCVDeposit',
-      'rariPool7FeiPCVDepositWrapper',
-      'rariPool9FeiPCVDepositWrapper',
-      'rariPool26FeiPCVDepositWrapper',
-      'rariPool28FeiPCVDepositWrapper',
-      'rariPool91FeiPCVDepositWrapper'
-    ],
-    category: ProposalCategory.DAO,
-    totalValue: 0,
-    proposal: fip_76b
-  },
-  fip_77: {
-    deploy: false,
-    proposalId: undefined,
-    affectedContractSignoff: ['tribalChief', 'fuseGuardian'],
-    deprecatedContractSignoff: [],
-    category: ProposalCategory.OA,
-    totalValue: 0,
-    proposal: fip_77
-  },
   fip_78a: {
     deploy: true,
     proposalId: undefined,
