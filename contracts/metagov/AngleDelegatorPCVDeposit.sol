d6038dcdf80cdc9674028ac3d3a53223eb828b3c
Erwan Beauvois <erwan@allmyt.eu>
2022-02-16 14:30:11 +0100

    Define constants in AngleDelegatorPCVDeposit

--- contracts/metagov/AngleDelegatorPCVDeposit.sol
+++ contracts/metagov/AngleDelegatorPCVDeposit.sol
@@ -10,6 +10,11 @@ import "./utils/OZGovernorVoter.sol";
 /// @author Fei Protocol
 contract AngleDelegatorPCVDeposit is SnapshotDelegatorPCVDeposit, VoteEscrowTokenManager, LiquidityGaugeManager, OZGovernorVoter {
 
+    address private constant ANGLE_TOKEN = 0x31429d1856aD1377A8A0079410B297e1a9e214c2;
+    address private constant ANGLE_VE_TOKEN = 0x0C462Dbb9EC8cD1630f1728B2CFD2769d09f0dd5;
+    address private constant ANGLE_GAUGE_MANAGER = 0x9aD7e7b0877582E14c17702EecF49018DD6f2367;
+    bytes32 private constant ANGLE_SNAPSHOT_SPACE = keccak256("anglegovernance.eth");
+
     /// @notice ANGLE token manager
     /// @param _core Fei Core for reference
     /// @param _initialDelegate initial delegate for snapshot votes
@@ -18,14 +23,14 @@ contract AngleDelegatorPCVDeposit is SnapshotDelegatorPCVDeposit, VoteEscrowToke
         address _initialDelegate
     ) SnapshotDelegatorPCVDeposit(
         _core,
-        IERC20(0x31429d1856aD1377A8A0079410B297e1a9e214c2), // ANGLE
-        keccak256("anglegovernance.eth"),
+        IERC20(ANGLE_TOKEN), // token used in reporting
+        ANGLE_SNAPSHOT_SPACE, // snapshot spaceId
         _initialDelegate
     ) VoteEscrowTokenManager(
-        IERC20(0x31429d1856aD1377A8A0079410B297e1a9e214c2), // ANGLE
-        IVeToken(0x0C462Dbb9EC8cD1630f1728B2CFD2769d09f0dd5), // veANGLE
-        4 * 365 * 86400 // 4 years
-    ) LiquidityGaugeManager(address(0x9aD7e7b0877582E14c17702EecF49018DD6f2367)) OZGovernorVoter() {}
+        IERC20(ANGLE_TOKEN), // liquid token
+        IVeToken(ANGLE_VE_TOKEN), // vote-escrowed token
+        4 * 365 * 86400 // vote-escrow max time = 4 years
+    ) LiquidityGaugeManager(ANGLE_GAUGE_MANAGER) OZGovernorVoter() {}
 
     /// @notice returns total balance of PCV in the Deposit
     function balance() public view override returns (uint256) {
