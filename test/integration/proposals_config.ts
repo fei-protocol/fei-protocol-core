8afb34517830d8e134b7c54602c9d70f7b594e9b
Erwan Beauvois <erwan@allmyt.eu>
2022-02-28 20:18:34 +0100

    Remove fip_84b and fip_84c from proposals config

--- test/integration/proposals_config.ts
+++ test/integration/proposals_config.ts
@@ -2,8 +2,6 @@ import { ProposalCategory, ProposalsConfigMap } from '@custom-types/types';
 
 // import fip_xx_proposal from '@proposals/description/fip_xx';
 import fip_78a from '@proposals/description/fip_78a';
-import fip_84b from '@proposals/description/fip_84b';
-import fip_84c from '@proposals/description/fip_84c';
 
 const proposals: ProposalsConfigMap = {
   /*
@@ -14,24 +12,6 @@ const proposals: ProposalsConfigMap = {
         proposal: fip_xx_proposal // full proposal file, imported from '@proposals/description/fip_xx.ts'
     }
     */
-  fip_84b: {
-    deploy: false,
-    proposalId: '111980608549296280462721500887998027192667619429469223121519018371053122326953',
-    affectedContractSignoff: ['proxyAdmin', 'timelock', 'feiDAO', 'feiDAOTimelock'],
-    deprecatedContractSignoff: [],
-    category: ProposalCategory.DAO,
-    totalValue: 0,
-    proposal: fip_84b
-  },
-  fip_84c: {
-    deploy: false,
-    proposalId: '34000709159909885586797325379867128929719243307711747917169184886926028102786',
-    affectedContractSignoff: ['timelock', 'rariTimelock'],
-    deprecatedContractSignoff: [],
-    category: ProposalCategory.DAO,
-    totalValue: 0,
-    proposal: fip_84c
-  },
   fip_78a: {
     deploy: false,
     proposalId: undefined,
