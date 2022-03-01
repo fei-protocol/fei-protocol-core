35e544691e93106170c11d1ad74d50968de1ddb9
Elliot <elliotfriedman3@gmail.com>
2022-02-28 17:25:10 -0800

    add pools, zero alloc points

--- protocol-configuration/tribalchief.ts
+++ protocol-configuration/tribalchief.ts
@@ -5,6 +5,14 @@ const config: TribalChiefConfig = {
   curve3Metapool: { allocPoint: 0, unlocked: false }, // Curve 3crv-FEI metapool LP
   gUniFeiDaiLP: { allocPoint: 0, unlocked: true }, // G-UNI DAI/FEI 0.05% fee tier
   stakingTokenWrapperRari: { allocPoint: 1000, unlocked: false }, // FeiRari: TRIBE
+  stakingTokenWrapperGROLaaS: { allocPoint: 0, unlocked: false }, // LaaS: GRO
+  stakingTokenWrapperFOXLaaS: { allocPoint: 0, unlocked: false }, // LaaS: FOX
+  stakingTokenWrapperUMALaaS: { allocPoint: 0, unlocked: false }, // LaaS: UMA
+  stakingTokenWrapperSYNLaaS: { allocPoint: 0, unlocked: false }, // LaaS: SYN
+  stakingTokenWrapperNEARLaaS: { allocPoint: 0, unlocked: false }, // LaaS: NEAR
+  stakingTokenWrapperKYLINLaaS: { allocPoint: 0, unlocked: false }, // LaaS: KYLIN
+  stakingTokenWrapperMStableLaaS: { allocPoint: 0, unlocked: false }, // LaaS: MStable
+  stakingTokenWrapperPoolTogetherLaaS: { allocPoint: 0, unlocked: false }, // LaaS: PoolTogether
   stakingTokenWrapperBribeD3pool: { allocPoint: 250, unlocked: false }, // Votium bribes: d3pool
   d3StakingTokenWrapper: { allocPoint: 250, unlocked: false }, // FeiRari: d3pool LP
   fei3CrvStakingtokenWrapper: { allocPoint: 1000, unlocked: false }, // FeiRari: 3crv-FEI metapool LP
