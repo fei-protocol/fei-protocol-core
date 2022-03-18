e339622d3e0e542348690530355e97e7bae679a0
Joey Santoro <joseph.santoro@duke.edu>
2022-03-18 16:07:35 -0700

    proposal description

--- proposals/description/fip_79.ts
+++ proposals/description/fip_79.ts
@@ -26,9 +26,16 @@ const fip_79: ProposalDescription = {
     }
   ],
   description: `
+    This proposal swaps 3.74M TRIBE for 577.18 gOHM with the Olympus DAO Treasury to form a protocol partnership.
+
+    As part of the partnership:
+    * Olympus will intake FEI to Treasury.
+    * Fei protocol to fill 20% portion of bond capacity
+    * Olympus to be included in Tribe Turbo
+
     Forum: https://tribe.fei.money/t/fip-79-olympus-dao-fei-partnership/3939/21
     Snapshot: https://snapshot.org/#/fei.eth/proposal/0x8fc10a0d66829e7e78b41a72d542adf7720f92b747a083400d77656744169907
-    Code: 
+    Code: https://github.com/fei-protocol/fei-protocol-core/pull/600
 `
 };
 
