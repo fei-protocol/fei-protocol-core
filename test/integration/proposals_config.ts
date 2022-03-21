8144a5239b6ac0e5c515515728ad28b29cee60b5
David Lucid <davidlucidsl@gmail.com>
2022-03-21 02:25:26 -0500

    Update proposals_config.ts

--- test/integration/proposals_config.ts
+++ test/integration/proposals_config.ts
@@ -2,6 +2,7 @@ import { ProposalCategory, ProposalsConfigMap } from '@custom-types/types';
 
 // import fip_x from '@proposals/description/fip_x';
 import fip_79 from '@proposals/description/fip_79';
+import fip_85 from '@proposals/description/fip_85';
 
 const proposals: ProposalsConfigMap = {
   /*fip_x: {
@@ -22,6 +23,15 @@ const proposals: ProposalsConfigMap = {
     category: ProposalCategory.DAO,
     totalValue: 0,
     proposal: fip_79
+  },
+  fip_85: {
+    deploy: false,
+    proposalId: null,
+    affectedContractSignoff: ['rariGovernanceProxyAdmin', 'rariGovernanceTokenSushiSwapDistributor', 'rariTimelock'],
+    deprecatedContractSignoff: [],
+    category: ProposalCategory.DAO,
+    totalValue: 0,
+    proposal: fip_85
   }
 };
 
