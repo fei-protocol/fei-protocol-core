a096a4c5cf933b4482dd1f978e79c80d0f2d67f0
David Lucid <davidlucidsl@gmail.com>
2022-03-22 03:11:42 -0500

    Lint FIP 85

--- proposals/description/fip_85.ts
+++ proposals/description/fip_85.ts
@@ -7,8 +7,9 @@ const fip_85: ProposalDescription = {
       target: 'rariGovernanceProxyAdmin',
       values: '0',
       method: 'upgrade(address,address)',
-      arguments: ["{rariGovernanceTokenSushiSwapDistributor}", "0x4650a83520be4f1e4cee726fdd169d37cea80525"],
-      description: 'Upgrade the RariGovernanceTokenUniswapDistributor to the new implementation (cuts off rewards at block 14476500)'
+      arguments: ['{rariGovernanceTokenSushiSwapDistributor}', '0x4650a83520be4f1e4cee726fdd169d37cea80525'],
+      description:
+        'Upgrade the RariGovernanceTokenUniswapDistributor to the new implementation (cuts off rewards at block 14476500)'
     },
     {
       target: 'rariGovernanceTokenSushiSwapDistributor',
@@ -21,8 +22,9 @@ const fip_85: ProposalDescription = {
       target: 'rariGovernanceTokenSushiSwapDistributor',
       values: '0',
       method: 'upgrade(address,uint256)',
-      arguments: ["{rariTimelock}", "404594685886433151695132"],
-      description: 'Forward the remaining RGT rewards to the timelock: 568717819057309757517546 - (568717819057309757517546 * 80 / 100 * (365 + 30) / (365 * 3))'
+      arguments: ['{rariTimelock}', '404594685886433151695132'],
+      description:
+        'Forward the remaining RGT rewards to the timelock: 568717819057309757517546 - (568717819057309757517546 * 80 / 100 * (365 + 30) / (365 * 3))'
     },
     {
       target: 'rariGovernanceTokenSushiSwapDistributor',
@@ -32,7 +34,7 @@ const fip_85: ProposalDescription = {
       description: 'Re-enable the RariGovernanceTokenUniswapDistributor'
     }
   ],
-  description: 'Kill RGT rewards on RGT/ETH SushiSwap pair--we don\'t need them anymore.'
+  description: "Kill RGT rewards on RGT/ETH SushiSwap pair--we don't need them anymore."
 };
 
 export default fip_85;
