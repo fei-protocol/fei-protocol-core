fe9ed61ca257d3ed68e0360934fbefffaf94ee1a
Caleb <ditchfieldcaleb@gmail.com>
2022-03-22 22:22:35 -0700

    update proposalid

--- test/integration/proposals_config.ts
+++ test/integration/proposals_config.ts
@@ -5,7 +5,7 @@ import fip_87 from '@proposals/description/fip_87';
 const proposals: ProposalsConfigMap = {
   fip_87: {
     deploy: false,
-    proposalId: null,
+    proposalId: '114110038939654197374429334111955717306290796780942705117190274082864935809950',
     affectedContractSignoff: [],
     deprecatedContractSignoff: [],
     category: ProposalCategory.DAO,
