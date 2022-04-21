9737d6e895f50920858765d09096272c955e8e5b
Joey Santoro <joseph.santoro@duke.edu>
2022-04-21 09:28:20 -0700

    release: clean proposals config

--- test/integration/proposals_config.ts
+++ test/integration/proposals_config.ts
@@ -1,17 +1,5 @@
 import { ProposalCategory, ProposalsConfigMap } from '@custom-types/types';
 
-import fip_94 from '@proposals/description/fip_94';
-
-const proposals: ProposalsConfigMap = {
-  fip_94: {
-    deploy: false,
-    proposalId: null,
-    affectedContractSignoff: ['rariTimelock'],
-    deprecatedContractSignoff: [],
-    category: ProposalCategory.DAO,
-    totalValue: 0,
-    proposal: fip_94
-  }
-};
+const proposals: ProposalsConfigMap = {};
 
 export default proposals;
