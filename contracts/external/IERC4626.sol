8ef6569ce577cf4ec238421b658c3dc3b188513c
Erwan Beauvois <erwan@allmyt.eu>
2022-04-22 15:36:28 +0200

    Remove virtual in IERC4626

--- contracts/external/IERC4626.sol
+++ contracts/external/IERC4626.sol
@@ -6,89 +6,68 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 /// @title ERC4626 interface
 /// See: https://eips.ethereum.org/EIPS/eip-4626
 interface IERC4626 is IERC20 {
-    function asset() external view virtual returns (address asset);
+    function asset() external view returns (address asset);
 
-    function totalAssets() external view virtual returns (uint256 totalAssets);
+    function totalAssets() external view returns (uint256 totalAssets);
 
     function deposit(uint256 assets, address receiver)
         external
-        virtual
         returns (uint256 shares);
 
     function mint(uint256 shares, address receiver)
         external
-        virtual
         returns (uint256 assets);
 
     function withdraw(
         uint256 assets,
         address receiver,
         address owner
-    ) external virtual returns (uint256 shares);
+    ) external returns (uint256 shares);
 
     function redeem(
         uint256 shares,
         address receiver,
         address owner
-    ) external virtual returns (uint256 assets);
+    ) external returns (uint256 assets);
 
     function convertToShares(uint256 assets)
         external
         view
-        virtual
         returns (uint256 shares);
 
     function convertToAssets(uint256 shares)
         external
         view
-        virtual
         returns (uint256 assets);
 
     function maxDeposit(address owner)
         external
         view
-        virtual
         returns (uint256 maxAssets);
 
     function maxWithdraw(address owner)
         external
         view
-        virtual
         returns (uint256 maxAssets);
 
-    function maxMint(address owner)
-        external
-        view
-        virtual
-        returns (uint256 maxShares);
+    function maxMint(address owner) external view returns (uint256 maxShares);
 
-    function maxRedeem(address owner)
-        external
-        view
-        virtual
-        returns (uint256 maxShares);
+    function maxRedeem(address owner) external view returns (uint256 maxShares);
 
     function previewDeposit(uint256 assets)
         external
         view
-        virtual
         returns (uint256 shares);
 
     function previewWithdraw(uint256 assets)
         external
         view
-        virtual
         returns (uint256 shares);
 
-    function previewMint(uint256 shares)
-        external
-        view
-        virtual
-        returns (uint256 assets);
+    function previewMint(uint256 shares) external view returns (uint256 assets);
 
     function previewRedeem(uint256 shares)
         external
         view
-        virtual
         returns (uint256 assets);
 }
