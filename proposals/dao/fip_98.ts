836fe463acc925de6d1ef9990318f89f946da0d3
Elliot <elliotfriedman3@gmail.com>
2022-04-25 15:10:42 -0400

    grammar fix

--- proposals/dao/fip_98.ts
+++ proposals/dao/fip_98.ts
@@ -22,7 +22,7 @@ Steps:
   to the VOLT PCV Deposit so that the funds do not get invested into fuse
 4. Add OPT as the oracle for Volt in the CR Oracle
 5. Add Volt ERC20PCVDepositWrapper to the CR Oracle so that the Volt value
-  will be reflected in the Tribe PCV
+  will be reflected in the PCV
 6. Add turboFusePCVDeposit as a safe address in the PCV Guardian
 */
 
