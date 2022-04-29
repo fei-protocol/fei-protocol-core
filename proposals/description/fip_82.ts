94bd44644b2d84b9a2d308b8fc220913b935fd56
thomas-waite <thomas.waite4@gmail.com>
2022-04-29 18:11:27 +0100

    feat: add snapshot election names to addresses

--- proposals/description/fip_82.ts
+++ proposals/description/fip_82.ts
@@ -182,15 +182,15 @@ const fip_82: ProposalDescription = {
   The Ethereum addresses of the 9 elected members are:
   
   [
-    '0xc8eefb8b3d50ca87Da7F99a661720148acf97EfA',
-    '0x72b7448f470D07222Dbf038407cD69CC380683F3',
-    '0xA6D08774604d6Da7C96684ca6c4f61f89c4e5b96',
-    '0xe0ac4559739bD36f0913FB0A3f5bFC19BCBaCD52',
-    '0xC2138f77E97A9Ac0A4bC26F42D80D29D1a091866',
-    '0x9f5e6F58CC8823D3c022AeBE3942EeF689E9AcD9',
-    '0xaB339ae6eab3C3CF4f5885E56F7B49391a01DDA6',
-    '0xd90E9181B20D8D1B5034d9f5737804Da182039F6',
-    '0x7671f0615B1764fb4bf4b8dF06B7338843f99678'
+    '0xc8eefb8b3d50ca87Da7F99a661720148acf97EfA', - Brianna Montgomery
+    '0x72b7448f470D07222Dbf038407cD69CC380683F3', - Fishy
+    '0xA6D08774604d6Da7C96684ca6c4f61f89c4e5b96', - Bruno Rodrigues
+    '0xe0ac4559739bD36f0913FB0A3f5bFC19BCBaCD52', - Joey Santoro
+    '0xC2138f77E97A9Ac0A4bC26F42D80D29D1a091866', - Jack Lipstone
+    '0x9f5e6F58CC8823D3c022AeBE3942EeF689E9AcD9', - Jack Longarzo
+    '0xaB339ae6eab3C3CF4f5885E56F7B49391a01DDA6', - State
+    '0xd90E9181B20D8D1B5034d9f5737804Da182039F6', - OneTrueKirk
+    '0x7671f0615B1764fb4bf4b8dF06B7338843f99678'  - Freddie Farmer
   ]
 
   There will be a second DAO vote to transfer various protocol roles to the TribalCouncil pod and to make it the admin of various roles - to allow it to 
