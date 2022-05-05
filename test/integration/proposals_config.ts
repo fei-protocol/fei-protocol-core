a78066bf971fc079ebaaf849aab93135e7a25bf5
thomas-waite <thomas.waite4@gmail.com>
2022-05-05 01:31:19 +0100

    refactor: update blockNumber to latest

--- test/integration/proposals_config.ts
+++ test/integration/proposals_config.ts
@@ -1,9 +1,6 @@
 import { ProposalCategory, ProposalsConfigMap } from '@custom-types/types';
 
-import fip_82 from '@proposals/description/fip_82';
 import fip_82b from '@proposals/description/fip_82b';
-import fip_98 from '@proposals/description/fip_98';
-import fip_99 from '@proposals/description/fip_99';
 
 // import fip_xx_proposal from '@proposals/description/fip_xx';
 
@@ -19,46 +16,6 @@ const proposals: ProposalsConfigMap = {
       category: ProposalCategory.DAO
     }
     */
-  fip_82: {
-    deploy: false, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
-    totalValue: 0, // amount of ETH to send to DAO execution
-    proposal: fip_82,
-    proposalId: '',
-    affectedContractSignoff: [
-      'roleBastion',
-      'podFactory',
-      'podExecutor',
-      'nopeDAO',
-      'governanceMetadataRegistry',
-      'core',
-      'tribe',
-      'feiDAOTimelock',
-      'tribalCouncilTimelock',
-      'tribalCouncilSafe',
-      'podAdminGateway'
-    ],
-    deprecatedContractSignoff: [],
-    category: ProposalCategory.DAO
-  },
-  fip_98: {
-    deploy: false,
-    proposalId: '47738997083165992958921925097327638388915944734650384828020246684763693471048',
-    affectedContractSignoff: [
-      'fei',
-      'feiDAOTimelock',
-      'voltFeiSwapContract',
-      'collateralizationOracle',
-      'volt',
-      'voltOracle',
-      'voltDepositWrapper',
-      'pcvGuardian',
-      'turboFusePCVDeposit'
-    ],
-    deprecatedContractSignoff: [],
-    category: ProposalCategory.DAO,
-    totalValue: 0,
-    proposal: fip_98
-  },
   fip_82b: {
     deploy: false, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
     totalValue: 0, // amount of ETH to send to DAO execution
