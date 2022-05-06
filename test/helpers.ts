1141478a9c9d17ae1ff44764010f845714fc7891
Caleb Ditchfield <ditchfieldcaleb@gmail.com>
2022-05-06 02:08:23 -0700

    special case for zero

--- test/helpers.ts
+++ test/helpers.ts
@@ -222,7 +222,11 @@ const time = {
 
     if (durationBN.lt(ethers.constants.Zero)) throw Error(`Cannot increase time by a negative amount (${duration})`);
 
-    await hre.network.provider.send('hardhat_mine', ['0x2', ethers.utils.hexStripZeros(durationBN.toHexString())]);
+    if (durationBN.eq(ethers.constants.Zero)) {
+      await hre.network.provider.send('hardhat_mine');
+    } else {
+      await hre.network.provider.send('hardhat_mine', ['0x2', ethers.utils.hexStripZeros(durationBN.toHexString())]);
+    }
   },
 
   increaseTo: async (target: number | string | BigNumberish): Promise<void> => {
