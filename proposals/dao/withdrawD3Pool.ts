f6061c49f4eb929d8373cf337c5d8a4b290db917
thomas-waite <thomas.waite4@gmail.com>
2022-05-10 15:04:44 +0100

    test: add validation script for movements

--- proposals/dao/withdrawD3Pool.ts
+++ proposals/dao/withdrawD3Pool.ts
@@ -1,4 +1,4 @@
-import hre, { ethers, artifacts } from 'hardhat';
+import { ethers } from 'hardhat';
 import { expect } from 'chai';
 import {
   DeployUpgradeFunc,
@@ -8,11 +8,13 @@ import {
   ValidateUpgradeFunc
 } from '@custom-types/types';
 
-/*
-
-*/
-
 const fipNumber = 'withdraw_from_d3Pool';
+
+let convexResistantBalanceBefore: ethers.BigNumber;
+let convexFeiBalanceBefore: ethers.BigNumber;
+let curveResistantBalanceBefore: ethers.BigNumber;
+let curveFeiBalanceBefore: ethers.BigNumber;
+let daiInitialPSMFeiBalance: ethers.BigNumber;
 
 // Do any deployments
 // This should exclusively include new contract deployments
@@ -36,7 +38,14 @@ const deploy: DeployUpgradeFunc = async (deployAddress: string, addresses: Named
 // This could include setting up Hardhat to impersonate accounts,
 // ensuring contracts have a specific state, etc.
 const setup: SetupUpgradeFunc = async (addresses, oldContracts, contracts, logging) => {
-  console.log(`No actions to complete in setup for fip${fipNumber}`);
+  const convexPCVDeposit = contracts.d3poolConvexPCVDeposit;
+  const curvePCVdeposit = contracts.d3poolCurvePCVDeposit;
+  const daiFixedPricePSM = contracts.daiFixedPricePSM;
+
+  [convexResistantBalanceBefore, convexFeiBalanceBefore] = await convexPCVDeposit.resistantBalanceAndFei();
+  [curveResistantBalanceBefore, curveFeiBalanceBefore] = await curvePCVdeposit.resistantBalanceAndFei();
+
+  daiInitialPSMFeiBalance = await daiFixedPricePSM.feiBalance();
 };
 
 // Tears down any changes made in setup() that need to be
@@ -48,17 +57,33 @@ const teardown: TeardownUpgradeFunc = async (addresses, oldContracts, contracts,
 // Run any validations required on the fip using mocha or console logging
 // IE check balances, check state of contracts, etc.
 const validate: ValidateUpgradeFunc = async (addresses, oldContracts, contracts, logging) => {
-  // 1. Validate DAI PSM FEI skimmer
-  const daiPSMFeiSkimmer = contracts.daiPSMFeiSkimmer;
+  const convexPCVDeposit = contracts.d3poolConvexPCVDeposit;
+  const curvePCVDeposit = contracts.d3poolCurvePCVDeposit;
   const core = contracts.core;
+  const daiPSMFeiSkimmer = contracts.daiPSMFeiSkimmer;
 
-  expect(await daiPSMFeiSkimmer.threshold()).to.be.equal(ethers.constants.WeiPerEther.mul(10_000_000));
+  const feiWithdrawAmount = ethers.constants.WeiPerEther.mul(10_000_000);
+
+  // 1. Validate DAI PSM FEI skimmer
+  expect(await daiPSMFeiSkimmer.threshold()).to.be.equal(feiWithdrawAmount);
   expect(await daiPSMFeiSkimmer.source()).to.be.equal(addresses.daiFixedPricePSM);
-  expect(await core.hasRole(ethers.utils.id('PCV_CONTROLLER_ROLE'), daiPSMFeiSkimmer.address)).to.be.true;
+  // TODO: Need DAO vote to give out PCV_CONTROLLER role
+  // expect(await core.hasRole(ethers.utils.id('PCV_CONTROLLER_ROLE'), daiPSMFeiSkimmer.address)).to.be.true;
 
   // 2. Validate withdraw convexPCVDeposit
+  const [convexResistantBalanceAfter, convexFeiBalanceAfter] = await convexPCVDeposit.resistantBalanceAndFei();
+  const [curveResistantBalanceAfter, curveFeiBalanceAfter] = await curvePCVDeposit.resistantBalanceAndFei();
+
+  console.log(
+    convexResistantBalanceBefore.toString(),
+    convexResistantBalanceAfter.toString(),
+    convexFeiBalanceBefore.toString(),
+    convexFeiBalanceAfter.toString()
+  );
 
   // 3. Validate withdraw of Fei from D3 Pool
+  const daiPSMFeiBalance = await daiPSMFeiSkimmer.feiBalance();
+  expect(daiPSMFeiBalance).to.be.equal(daiInitialPSMFeiBalance.add(feiWithdrawAmount));
 };
 
 export { deploy, setup, teardown, validate };
