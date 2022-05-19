a4596b30e9d08cdd0af4839e500b72841a4895a4
Joey <31974730+Joeysantoro@users.noreply.github.com>
2022-05-19 16:15:26 -0700

    Clear proposals config

--- test/integration/proposals_config.ts
+++ test/integration/proposals_config.ts
@@ -1,7 +1,5 @@
 import { ProposalCategory, ProposalsConfigMap } from '@custom-types/types';
 
-import fip_82b from '@proposals/description/fip_82b';
-
 // import fip_xx_proposal from '@proposals/description/fip_xx';
 
 const proposals: ProposalsConfigMap = {
@@ -16,36 +14,6 @@ const proposals: ProposalsConfigMap = {
       category: ProposalCategory.DAO
     }
     */
-  fip_82b: {
-    deploy: false, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
-    totalValue: 0, // amount of ETH to send to DAO execution
-    proposal: fip_82b,
-    proposalId: '100114570810112501972314307970004446869175784368342294400079771147860837319881',
-    affectedContractSignoff: [
-      'core',
-      'fuseGuardian',
-      'optimisticMinter',
-      'pcvEquityMinter',
-      'indexDelegator',
-      'ethTokemakPCVDeposit',
-      'uniswapPCVDeposit',
-      'lusdPSMFeiSkimmer',
-      'ethPSMFeiSkimmer',
-      'aaveEthPCVDripController',
-      'daiPCVDripController',
-      'lusdPCVDripController',
-      'tribalCouncilTimelock',
-      'feiDAOTimelock',
-      'roleBastion',
-      'pcvGuardianNew',
-      'pcvGuardian',
-      'opsOptimisticTimelock',
-      'optimisticTimelock',
-      'tribalChiefSyncV2'
-    ],
-    deprecatedContractSignoff: [],
-    category: ProposalCategory.DAO
-  }
 };
 
 export default proposals;
