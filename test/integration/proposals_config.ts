b93fd669f15e4eea641ed8643ccd8d64753a2e23
thomas-waite <thomas.waite4@gmail.com>
2022-05-23 15:25:24 +0100

    refactor: remove executed proposal, update block num

--- test/integration/proposals_config.ts
+++ test/integration/proposals_config.ts
@@ -1,50 +1,9 @@
 import { ProposalCategory, ProposalsConfigMap } from '@custom-types/types';
 
-import fip_104 from '@proposals/description/fip_104';
 import balancer_gauge_fix from '@proposals/description/balancer_gauge_fix';
 import withdraw_lbp_liquidity from '@proposals/description/withdraw_lbp_liquidity';
 
 const proposals: ProposalsConfigMap = {
-  fip_104: {
-    deploy: false, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
-    totalValue: 0, // amount of ETH to send to DAO execution
-    proposal: fip_104, // full proposal file, imported from '@proposals/description/fip_xx.ts'
-    proposalId: '',
-    affectedContractSignoff: [
-      'daiFixedPricePSMFeiSkimmer',
-      'core',
-      'compoundDaiPCVDeposit',
-      'dpiToDaiLBPSwapper',
-      'dpiToDaiLensDai',
-      'dpiToDaiLensDpi',
-      'collateralizationOracle',
-      'tribalCouncilTimelock',
-      'tribalCouncilSafe',
-      'nopeDAO',
-      'compoundEthPCVDeposit',
-      'aaveEthPCVDeposit',
-      'pcvGuardianNew',
-      'uniswapPCVDeposit'
-    ],
-    deprecatedContractSignoff: [
-      'rariPool31FeiPCVDepositWrapper',
-      'rariPool25FeiPCVDepositWrapper',
-      'rariPool9RaiPCVDepositWrapper',
-      'aaveRaiPCVDepositWrapper',
-      'rariPool19DpiPCVDepositWrapper',
-      'liquityFusePoolLusdPCVDeposit',
-      'rariPool72FeiPCVDepositWrapper',
-      'raiDepositWrapper',
-      'rariPool31FeiPCVDeposit',
-      'rariPool25FeiPCVDeposit',
-      'rariPool9RaiPCVDeposit',
-      'aaveRaiPCVDeposit',
-      'rariPool19DpiPCVDeposit',
-      'rariPool72FeiPCVDeposit',
-      'dpiDepositWrapper'
-    ],
-    category: ProposalCategory.DAO
-  },
   balancer_gauge_fix: {
     deploy: false, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
     totalValue: 0, // amount of ETH to send to DAO execution
