5566580e47f2e0cad8e161afea15dc6156add656
thomas-waite <thomas.waite4@gmail.com>
2022-05-23 20:07:15 +0100

    refactor: pull DAI from Compound

--- proposals/description/curve_swap_stables.ts
+++ proposals/description/curve_swap_stables.ts
@@ -4,11 +4,18 @@ const curve_swap_stables: ProposalDescription = {
   title: 'Curve swap stables',
   commands: [
     {
+      target: 'pcvGuardianNew',
+      values: '0',
+      method: 'withdrawToSafeAddress(address,address,uint256,bool,bool)',
+      arguments: ['{compoundDaiPCVDeposit}', '{tribalCouncilTimelock}', '10500000000000000000000000', false, false],
+      description: 'Withdraw 10.5M DAI from the Compound DAI PCV deposit to the Tribal Council timelock'
+    },
+    {
       target: 'dai',
       values: '0',
       method: 'approve(address,uint256)',
-      arguments: ['{curve3pool}', '12000000000000000000000000'],
-      description: 'Approve 12M DAI for Curve 3Pool swaps'
+      arguments: ['{curve3pool}', '10500000000000000000000000'],
+      description: 'Approve 10.5M DAI for Curve 3Pool swaps'
     },
     {
       target: 'curve3pool',
@@ -32,17 +39,10 @@ const curve_swap_stables: ProposalDescription = {
         '134000000000000000000000', // dx = 134k DAI
         '134000000000' // min_dy = 134k USDT
       ],
-      description: 'Swap DAI for 134k DAI for 134k USDT via the 3Pool'
-    },
-    {
-      target: '',
-      values: '0',
-      method: 'exchange()',
-      arguments: [],
-      description: 'Swap DAI for USDC via the 3Pool'
+      description: 'Swap 134k DAI for 134k USDT via the 3Pool'
     }
   ],
-  description: 'Swap DAI for USDC, USDT and USTw on Curve'
+  description: 'Swap DAI for USDC and USDT on Curve'
 };
 
 export default curve_swap_stables;
