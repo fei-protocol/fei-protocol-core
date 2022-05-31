f45d62a15b010314555f3545de87fae373f4c166
thomas-waite <thomas.waite4@gmail.com>
2022-05-31 22:53:00 +0100

    refactor: re-add new roles

--- test/integration/proposals_config.ts
+++ test/integration/proposals_config.ts
@@ -1,7 +1,17 @@
 import { ProposalCategory, ProposalsConfigMap } from '@custom-types/types';
 import repay_fuse_bad_debt from '@proposals/description/repay_fuse_bad_debt';
+import register_proposal from '@proposals/description/register_proposal';
 
 const proposals: ProposalsConfigMap = {
+  register_proposal: {
+    deploy: false, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
+    totalValue: 0, // amount of ETH to send to DAO execution
+    proposal: register_proposal, // full proposal file, imported from '@proposals/description/fip_xx.ts'
+    proposalId: '',
+    affectedContractSignoff: [],
+    deprecatedContractSignoff: [''],
+    category: ProposalCategory.TC
+  },
   repay_fuse_bad_debt: {
     deploy: false, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
     totalValue: 0, // amount of ETH to send to DAO execution
