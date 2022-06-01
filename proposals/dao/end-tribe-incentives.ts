d1bff39e5f0f50678faaccf30d293da835f2fd13
thomas-waite <thomas.waite4@gmail.com>
2022-06-01 17:22:37 +0100

    feat: initial proposal setup

---
+++ proposals/dao/end-tribe-incentives.ts
@@ -0,0 +1,58 @@
+import hre, { ethers, artifacts } from 'hardhat';
+import { expect } from 'chai';
+import {
+  DeployUpgradeFunc,
+  NamedAddresses,
+  SetupUpgradeFunc,
+  TeardownUpgradeFunc,
+  ValidateUpgradeFunc
+} from '@custom-types/types';
+
+/*
+
+TIP-109: Discontinue Tribe Incentives
+
+Ends all Tribe Incentives being distributed
+
+Steps:
+1. Mass update all pools registered for Tribe rewards, so they have all rewards they are entitled to so far
+2. Set TribalChief block reward to zero, to stop distributing new rewards
+3. Calculate how much TRIBE the reward receivers have accrued, x
+4. Withdraw balance(TribalChief) - x amount of Tribe from the TribalChief contract
+5. Stop displaying Tribe rewards on the UIs:
+   - 
+  
+
+*/
+
+const fipNumber = 'TIP-109: Discontinue Tribe Incentives';
+
+// Do any deployments
+// This should exclusively include new contract deployments
+const deploy: DeployUpgradeFunc = async (deployAddress: string, addresses: NamedAddresses, logging: boolean) => {
+  console.log(`No deploy actions for fip${fipNumber}`);
+  return {
+    // put returned contract objects here
+  };
+};
+
+// Do any setup necessary for running the test.
+// This could include setting up Hardhat to impersonate accounts,
+// ensuring contracts have a specific state, etc.
+const setup: SetupUpgradeFunc = async (addresses, oldContracts, contracts, logging) => {
+  console.log(`No actions to complete in setup for fip${fipNumber}`);
+};
+
+// Tears down any changes made in setup() that need to be
+// cleaned up before doing any validation checks.
+const teardown: TeardownUpgradeFunc = async (addresses, oldContracts, contracts, logging) => {
+  console.log(`No actions to complete in teardown for fip${fipNumber}`);
+};
+
+// Run any validations required on the fip using mocha or console logging
+// IE check balances, check state of contracts, etc.
+const validate: ValidateUpgradeFunc = async (addresses, oldContracts, contracts, logging) => {
+  console.log(`No actions to complete in validate for fip${fipNumber}`);
+};
+
+export { deploy, setup, teardown, validate };
---
+++ proposals/description/end-tribe-incentives.ts
@@ -0,0 +1,32 @@
+import { ProposalDescription } from '@custom-types/types';
+
+const end_tribe_incentives: ProposalDescription = {
+  // Pool ID is where the pool is in the array
+  title: 'TIP-109: Discontinue Tribe Incentives',
+  commands: [
+    {
+      target: 'tribalChief',
+      values: '0',
+      method: 'massUpdatePools(uint256[])',
+      arguments: [['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17']],
+      description: 'Update reward variables on all pools registered for Tribe rewards'
+    },
+    {
+      target: 'tribalChief',
+      values: '0',
+      method: 'updateBlockReward(uint256)',
+      arguments: ['0'],
+      description: 'Set Tribal Chief block reward to 0'
+    }
+    // TODO: Harvest deposits
+    // TODO: Sync up deposits
+  ],
+  description: `
+  TIP-109: Discontinue Tribe Incentives
+
+  This proposal will disable all Tribe incentives. It updates the reward variables of incentivised pools and then
+  sets the amount of Tribe issued per block by the Tribal Chief to 0.
+  `
+};
+
+export default end_tribe_incentives;
