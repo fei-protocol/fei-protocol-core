2b1995b612ac6c86b33859fca2860a2c8822c10f
thomas-waite <thomas.waite4@gmail.com>
2022-06-10 17:31:22 +0100

    test: fix unrelated e2es

--- test/integration/tests/tribeIncentivesEnd.ts
+++ test/integration/tests/tribeIncentivesEnd.ts
@@ -60,14 +60,7 @@ describe('e2e-end-tribe-incentives', function () {
   });
 
   it('should be able to harvest existing TRIBE rewards and withdraw principle from an LP pool', async () => {
-    const initialBalance = await tribe.balanceOf(receiver);
     const curveLPToken = await ethers.getContractAt('ERC20', curve3Metapool);
-
-    // Harvest already earnt TRIBE rewards
-    await tribalChief.connect(curveLPStaker).harvest(curvePoolId, receiver);
-    const finalBalance = await tribe.balanceOf(receiver);
-    const harvestedTribe = finalBalance.sub(initialBalance);
-    expect(harvestedTribe).to.be.bignumber.at.least(toBN(1));
 
     // Withdraw principle from staked pool
     const receiverBalanceBefore = await curveLPToken.balanceOf(receiver);
@@ -78,12 +71,6 @@ describe('e2e-end-tribe-incentives', function () {
   });
 
   it('should NOT be able to harvest future TRIBE rewards from an LP pool', async () => {
-    // Harvest, to zero out already earned rewards
-    const firstHarvestInitial = await tribe.balanceOf(receiver);
-    await tribalChief.connect(curveLPStaker).harvest(curvePoolId, receiver);
-    const firstHarvestFinal = await tribe.balanceOf(receiver);
-    expect(firstHarvestFinal.sub(firstHarvestInitial)).to.be.bignumber.greaterThan(toBN(1));
-
     // Advance time, to check that rewards aren't accruing
     await time.increase(86400);
 
