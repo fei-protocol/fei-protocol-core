afe82332e5810732e09d1264f18500e4b05b2b89
thomas-waite <thomas.waite4@gmail.com>
2022-06-12 18:30:02 +0100

    refactor: emit proposalId instead of payload hash

--- contracts/pods/PodExecutor.sol
+++ contracts/pods/PodExecutor.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.0;
 
-import {ITimelock} from "../dao/timelock/ITimelock.sol";
+import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
 import {CoreRef} from "../refs/CoreRef.sol";
 
 /// @title PodExecutor
-/// @notice Executor gateway contract that allows any address to execute prepared timelock transactions
-/// @dev Access is granted to this contract to execute transactions on a timelock
+/// @notice Executor gateway contract which exposes the execution of prepared timelock transactions and makes them public
+/// @dev EXECUTOR_ROLE must be granted to this contract by the relevant timelock, in order for this contract to execute
 contract PodExecutor is CoreRef {
     event ExecuteTransaction(address timelock, bytes32 dataHash);
 
@@ -21,9 +21,15 @@ contract PodExecutor is CoreRef {
         bytes32 predecessor,
         bytes32 salt
     ) public payable whenNotPaused {
-        bytes32 dataPayloadHash = keccak256(data);
-        ITimelock(timelock).execute(target, value, data, predecessor, salt);
-        emit ExecuteTransaction(timelock, dataPayloadHash);
+        bytes32 proposalId = TimelockController(payable(timelock)).hashOperation(
+            target,
+            value,
+            data,
+            predecessor,
+            salt
+        );
+        TimelockController(payable(timelock)).execute(target, value, data, predecessor, salt);
+        emit ExecuteTransaction(timelock, proposalId);
     }
 
     /// @notice Batch execute transactions on a set of timelocks. Must have EXECUTE_ROLE on the appropriate timelocks
