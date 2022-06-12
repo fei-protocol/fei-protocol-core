9bd84c7876f0eb9d0e54682ddbd1e13fc609d3bb
thomas-waite <thomas.waite4@gmail.com>
2022-06-12 14:39:28 +0100

    refactor: update block num and proposals config

--- protocol-configuration/proposalsConfig.ts
+++ protocol-configuration/proposalsConfig.ts
@@ -1,21 +1,10 @@
 import { ProposalCategory, TemplatedProposalsConfigMap } from '@custom-types/types';
 import repay_fuse_bad_debt from '@proposals/description/repay_fuse_bad_debt';
-import end_tribe_incentives from '@proposals/description/end_tribe_incentives';
 import tokemak_withdraw from '@proposals/description/tokemak_withdraw';
-import eth_lbp from '@proposals/description/eth_lbp';
 import fip_104b from '@proposals/description/fip_104b';
 import clawback from '@proposals/description/clawback';
 
 const proposals: TemplatedProposalsConfigMap = {
-  eth_lbp: {
-    deploy: false, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
-    totalValue: 0, // amount of ETH to send to DAO execution
-    proposal: eth_lbp, // full proposal file, imported from '@proposals/description/fip_xx.ts'
-    proposalId: '',
-    affectedContractSignoff: [],
-    deprecatedContractSignoff: [],
-    category: ProposalCategory.TC
-  },
   repay_fuse_bad_debt: {
     deploy: false, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
     totalValue: 0, // amount of ETH to send to DAO execution
