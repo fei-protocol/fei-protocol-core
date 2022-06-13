9fa68f7f87dee0453907328c1849cc7323ac4902
Erwan Beauvois <erwan@allmyt.eu>
2022-06-13 18:23:43 +0200

    fix default parameter value in CompositeOracle unit tests

--- test/unit/oracle/CompositeOracle.test.ts
+++ test/unit/oracle/CompositeOracle.test.ts
@@ -42,7 +42,7 @@ describe('CompositeOracle', function () {
 
     this.oracle = await (
       await ethers.getContractFactory('CompositeOracle')
-    ).deploy(this.core.address, this.oracleA.address, this.oracleB.address, false);
+    ).deploy(this.core.address, this.oracleA.address, this.oracleB.address, true);
   });
 
   describe('Init', function () {
