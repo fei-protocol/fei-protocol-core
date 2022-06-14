acc8fbdbc47ebcc9184ebf49e6ac355e1cd53b7e
Caleb <ditchfieldcaleb@gmail.com>
2022-06-14 16:35:25 -0700

    Adjust proposalsConfig

--- protocol-configuration/proposalsConfig.ts
+++ protocol-configuration/proposalsConfig.ts
@@ -13,15 +13,6 @@ const proposals: TemplatedProposalsConfigMap = {
     deprecatedContractSignoff: [],
     category: ProposalCategory.DAO
   },
-  clawback: {
-    deploy: false, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
-    totalValue: 0, // amount of ETH to send to DAO execution
-    proposal: clawback, // full proposal file, imported from '@proposals/description/fip_xx.ts'
-    proposalId: '',
-    affectedContractSignoff: [],
-    deprecatedContractSignoff: [],
-    category: ProposalCategory.DAO
-  },
   tokemak_withdraw: {
     deploy: false, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
     totalValue: 0, // amount of ETH to send to DAO execution
