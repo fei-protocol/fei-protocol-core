d8076576da45a96a9aab361cc73ec77d6c815f0c
thomas-waite <thomas.waite4@gmail.com>
2022-06-15 21:15:35 +0100

    refactor: do not deploy contracts in pod exec

--- protocol-configuration/proposalsConfig.ts
+++ protocol-configuration/proposalsConfig.ts
@@ -38,7 +38,7 @@ const proposals: TemplatedProposalsConfigMap = {
     category: ProposalCategory.TC
   },
   pod_exec_v2: {
-    deploy: true, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
+    deploy: false, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
     totalValue: 0, // amount of ETH to send to DAO execution
     proposal: pod_exec_v2, // full proposal file, imported from '@proposals/description/fip_xx.ts'
     proposalId: '',
