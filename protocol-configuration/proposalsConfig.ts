0e2ac060bfbacad5679c7da69e86c6dae68b71a2
Joey Santoro <joseph.santoro@duke.edu>
2022-06-27 17:00:21 -0700

    uncomment proposals config

--- protocol-configuration/proposalsConfig.ts
+++ protocol-configuration/proposalsConfig.ts
@@ -11,16 +11,16 @@ const proposals: TemplatedProposalsConfigMap = {
     affectedContractSignoff: [],
     deprecatedContractSignoff: [],
     category: ProposalCategory.None
+  },
+  tip_117: {
+    deploy: false, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
+    totalValue: 0, // amount of ETH to send to DAO execution
+    proposal: tip_117, // full proposal file, imported from '@proposals/description/fip_xx.ts'
+    proposalId: '',
+    affectedContractSignoff: [],
+    deprecatedContractSignoff: [],
+    category: ProposalCategory.DAO
   }
-  // tip_117: {
-  //   deploy: false, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
-  //   totalValue: 0, // amount of ETH to send to DAO execution
-  //   proposal: tip_117, // full proposal file, imported from '@proposals/description/fip_xx.ts'
-  //   proposalId: '',
-  //   affectedContractSignoff: [],
-  //   deprecatedContractSignoff: [],
-  //   category: ProposalCategory.DAO
-  // },
 };
 
 export default proposals;
