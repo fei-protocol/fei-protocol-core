443244659dc01811f9e9b270be43a58f2bcdb4ad
Joey Santoro <joseph.santoro@duke.edu>
2022-06-29 09:22:06 -0700

    fip

--- protocol-configuration/proposalsConfig.ts
+++ protocol-configuration/proposalsConfig.ts
@@ -1,10 +1,12 @@
 import { ProposalCategory, TemplatedProposalDescription, TemplatedProposalsConfigMap } from '@custom-types/types';
 
+import fip_x from '@proposals/description/fip_x';
+
 const proposals: TemplatedProposalsConfigMap = {
   fuse_withdrawal_guard: {
     deploy: true, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
     totalValue: 0, // amount of ETH to send to DAO execution
-    proposal: tip_117, // full proposal file, imported from '@proposals/description/fip_xx.ts'
+    proposal: fip_x as unknown as TemplatedProposalDescription, // temporary hacky workaround, this field isn't needed but can't be undefined
     proposalId: '',
     affectedContractSignoff: [],
     deprecatedContractSignoff: [],
