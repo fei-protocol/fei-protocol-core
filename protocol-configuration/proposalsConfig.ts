f624271afc970667a20f919be2c0d01ced178129
Joey Santoro <joseph.santoro@duke.edu>
2022-06-29 11:03:18 -0700

    add deployed contract

--- protocol-configuration/proposalsConfig.ts
+++ protocol-configuration/proposalsConfig.ts
@@ -2,16 +2,6 @@ import { ProposalCategory, TemplatedProposalDescription, TemplatedProposalsConfi
 
 import fip_x from '@proposals/description/fip_x';
 
-const proposals: TemplatedProposalsConfigMap = {
-  fuse_withdrawal_guard: {
-    deploy: true, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
-    totalValue: 0, // amount of ETH to send to DAO execution
-    proposal: fip_x as unknown as TemplatedProposalDescription, // temporary hacky workaround, this field isn't needed but can't be undefined
-    proposalId: '',
-    affectedContractSignoff: [],
-    deprecatedContractSignoff: [],
-    category: ProposalCategory.None
-  }
-};
+const proposals: TemplatedProposalsConfigMap = {};
 
 export default proposals;
