43e9873a8bd0d03a2486569d2ee580cecf1c3de0
thomas-waite <thomas.waite4@gmail.com>
2022-07-01 18:48:04 +0100

    feat: add proposal scripts

---
+++ proposals/description/tip_119.ts
@@ -0,0 +1,30 @@
+import { TemplatedProposalDescription } from '@custom-types/types';
+
+const tip_119: TemplatedProposalDescription = {
+  title: 'TIP-119: Add gOHM to Collaterisation Oracle',
+  commands: [
+    {
+      target: 'collateralizationOracle',
+      values: '0',
+      method: 'setOracle(address,address)',
+      arguments: (addresses) => [addresses.gohm, addresses.gOhmUSDOracle],
+      description: 'Set the gOHM USD oracle on the Collaterisation Oracle, to add the gOHM holding deposit'
+    },
+    {
+      target: 'collateralizationOracle',
+      values: '0',
+      method: 'addDeposit(address)',
+      arguments: (addresses) => [addresses.gOHMPCVHoldingDeposit],
+      description: 'Add the gOHM Holding PCV Deposit'
+    }
+  ],
+  description: `
+  TIP-119: Add gOHM to Collaterisation Oracle
+
+  Register the gOHM USD oracle on the Collaterisation Oracle.
+
+  Then register the gOHM Holding PCV Deposit.
+  `
+};
+
+export default tip_119;
--- protocol-configuration/proposalsConfig.ts
+++ protocol-configuration/proposalsConfig.ts
@@ -1,7 +1,18 @@
-import { ProposalCategory, TemplatedProposalDescription, TemplatedProposalsConfigMap } from '@custom-types/types';
+import { ProposalCategory, TemplatedProposalsConfigMap } from '@custom-types/types';
 
 import fip_x from '@proposals/description/fip_x';
+import tip_119 from '@proposals/description/tip_119';
 
-const proposals: TemplatedProposalsConfigMap = {};
+const proposals: TemplatedProposalsConfigMap = {
+  tip_119: {
+    deploy: true, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
+    totalValue: 0, // amount of ETH to send to DAO execution
+    proposal: tip_119, // full proposal file, imported from '@proposals/description/fip_xx.ts'
+    proposalId: '',
+    affectedContractSignoff: [],
+    deprecatedContractSignoff: [],
+    category: ProposalCategory.DAO
+  }
+};
 
 export default proposals;
