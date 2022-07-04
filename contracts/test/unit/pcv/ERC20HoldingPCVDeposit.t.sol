92711656c260e9193ae5d5dd5736fefbc5c0dacc
thomas-waite <thomas.waite4@gmail.com>
2022-07-04 15:06:24 +0100

    style: improve test dir structure

--- contracts/test/unit/pcv/ERC20HoldingPCVDeposit.t.sol
+++ contracts/test/unit/pcv/ERC20HoldingPCVDeposit.t.sol
@@ -2,12 +2,12 @@ pragma solidity ^0.8.4;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-import {Vm} from "../utils/Vm.sol";
-import {DSTest} from "../utils/DSTest.sol";
-import {ERC20HoldingPCVDeposit} from "../../pcv/ERC20HoldingPCVDeposit.sol";
-import {Core} from "../../core/Core.sol";
-import {MockERC20} from "../../mock/MockERC20.sol";
-import {getCore, getAddresses, FeiTestAddresses} from "../utils/Fixtures.sol";
+import {Vm} from "../../utils/Vm.sol";
+import {DSTest} from "../../utils/DSTest.sol";
+import {ERC20HoldingPCVDeposit} from "../../../pcv/ERC20HoldingPCVDeposit.sol";
+import {Core} from "../../../core/Core.sol";
+import {MockERC20} from "../../../mock/MockERC20.sol";
+import {getCore, getAddresses, FeiTestAddresses} from "../../utils/Fixtures.sol";
 
 contract ERC20HoldingPCVDepositTest is DSTest {
     ERC20HoldingPCVDeposit private emptyDeposit;
