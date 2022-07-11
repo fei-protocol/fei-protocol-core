5f30504e385cb98d3da04a33a9ca7cde75f816fe
Caleb <ditchfieldcaleb@gmail.com>
2022-07-10 22:30:23 -0700

    Fix more errors

--- test/integration/tests/collateralizationOracle.ts
+++ test/integration/tests/collateralizationOracle.ts
@@ -109,6 +109,7 @@ describe('e2e-collateralization', function () {
       const collateralizationOracle = contracts.collateralizationOracle;
 
       for (const entry of Object.entries(CollateralizationOracleConfig)) {
+        console.log(`Checking entry entry: ${entry[0]}:${entry[1]}`);
         const expectedEntryCount = entry[1].length;
         const deposits = await collateralizationOracle.getDepositsForToken(entry[0]);
         const actualEntryCount = deposits.length;
