e420a0bb1a283259ae647d1ac808c6ca499d5627
thomas-waite <thomas.waite4@gmail.com>
2022-07-13 17:54:19 +0100

    test: unit tests for GovernorCountingFor

--- contracts/test/unit/governance/GovernorCountingFor.sol
+++ contracts/test/unit/governance/GovernorCountingFor.sol
@@ -38,13 +38,13 @@ function createDummyProposal(address ethReceiver, uint256 ethAmount)
 
 /// @dev Validates vote counting functionality of the GovernorCountingFor module. Module is abstract
 ///      so instantiated as a NopeDAO
-contract GovernorCountingFor is DSTest {
+contract GovernorCountingForTest is DSTest {
     address userWithQuorumTribe = address(0x1);
-    address userWithInsufficientTribe = address(0x2);
-    address userWithZeroTribe = address(0x3);
+    address userWith5MTribe = address(0x2);
+    address userWith2MTribe = address(0x3);
+    address userWithZeroTribe = address(0x4);
 
     uint256 excessQuorumTribe = (11e6) * (10**18);
-    address tribeAddress;
     ERC20VotesComp tribe;
 
     Vm public constant vm = Vm(HEVM_ADDRESS);
@@ -58,23 +58,27 @@ contract GovernorCountingFor is DSTest {
         // 1. Setup core
         core = getCore();
 
-        // 2. Setup Tribe and transfer some to a test address
-        tribeAddress = address(core.tribe());
-
+        // 2. Setup Tribe, grant to test addresses and delegate
         vm.startPrank(addresses.governorAddress);
         core.allocateTribe(userWithQuorumTribe, excessQuorumTribe);
-        core.allocateTribe(userWithInsufficientTribe, 5e6 * (10**18));
+        core.allocateTribe(userWith5MTribe, 5e6 * (10**18));
+        core.allocateTribe(userWith2MTribe, 2e6 * (10**18));
         vm.stopPrank();
 
-        tribe = ERC20VotesComp(tribeAddress);
+        tribe = ERC20VotesComp(address(core.tribe()));
 
+        // Delegate TRIBE to users, allowing them to vote
         vm.prank(userWithQuorumTribe);
         tribe.delegate(userWithQuorumTribe);
 
+        vm.prank(userWith5MTribe);
+        tribe.delegate(userWith5MTribe);
+
+        vm.prank(userWith2MTribe);
+        tribe.delegate(userWith2MTribe);
+
         // 3. Deploy NopeDAO
         nopeDAO = new NopeDAO(tribe, address(core));
-
-        vm.roll(block.number + 1); // Make block number non-zero, for getVotes accounting
 
         // 4. Seed NopeDAO with ETH so in theory could execute the proposal
         vm.deal(address(nopeDAO), 10 ether);
@@ -85,10 +89,11 @@ contract GovernorCountingFor is DSTest {
             uint256[] memory values,
             bytes[] memory calldatas,
             string memory description,
-            bytes32 descriptionHash
+
         ) = createDummyProposal(address(this), 1 ether);
 
         proposalId = nopeDAO.propose(targets, values, calldatas, description);
+        vm.roll(block.number + 1); // Make block number non-zero, for getVotes accounting
     }
 
     // enum ProposalState {
@@ -107,29 +112,56 @@ contract GovernorCountingFor is DSTest {
         assertEq(nopeDAO.COUNTING_MODE(), "quorum=bravo");
         assertEq(nopeDAO.proposalVotes(proposalId), 0);
         assertEq(nopeDAO.hasVoted(proposalId, address(this)), false);
-        assertEq(uint8(nopeDAO.state(proposalId)), 0);
+        assertEq(uint8(nopeDAO.state(proposalId)), 1);
+        assertEq(nopeDAO.proposalThreshold(), 0);
+
+        // Verify voter allocated expected votes
+        assertEq(nopeDAO.getVotes(userWith5MTribe, block.number - 1), 5e6 * (10**18));
+        assertEq(nopeDAO.getVotes(userWith2MTribe, block.number - 1), 2e6 * (10**18));
     }
+
 
     /// @notice Validate that a FOR vote is counted
     function testCountForVote() public {
-        vm.prank(userWithQuorumTribe);
+        vm.prank(userWith5MTribe);
         nopeDAO.castVote(proposalId, 1);
 
-        // Verify proposal state and vote balance
-        assertEq(uint8(nopeDAO.state(proposalId)), 0);
+        // Verify proposal state
+        assertEq(uint8(nopeDAO.state(proposalId)), 1);
 
-        assertEq(uint8(nopeDAO.state(proposalId)), 0);
+        // Verify voting accounting
+        assertTrue(nopeDAO.hasVoted(proposalId, userWith5MTribe));
+        assertEq(nopeDAO.proposalVotes(proposalId), 5e6 * (10**18));
     }
 
-    /// @notice Validate that multiple FOR votes are counted
-    function testCountMultipleForVotes() public {}
+    /// @notice Validate arithmetic of counting multiple FOR votes
+    function testCountMultipleForVotes() public {
+        vm.prank(userWith5MTribe);
+        nopeDAO.castVote(proposalId, 1);
+
+        vm.prank(userWith2MTribe);
+        nopeDAO.castVote(proposalId, 1);
+
+        // Verify proposal state
+        assertEq(uint8(nopeDAO.state(proposalId)), 1);
+
+        // Verify voting accounting
+        assertTrue(nopeDAO.hasVoted(proposalId, userWith5MTribe));
+        assertTrue(nopeDAO.hasVoted(proposalId, userWith2MTribe));
+        assertEq(nopeDAO.proposalVotes(proposalId), (5e6 + 2e6) * (10**18));
+    }
 
     /// @notice Validate that a non-FOR vote is rejected
-    function testRejectsNonForVote() public {}
-
-    /// @notice Validate that votes are always counted as having succeeded
-    function testVoteSucceededAlwaysTrue() public {}
+    function testRejectsNonForVote() public {
+        vm.prank(userWith5MTribe);
+        vm.expectRevert(bytes("GovernorCountingFor: only FOR votes supported"));
+        nopeDAO.castVote(proposalId, 0);
+    }
 
     /// @notice Validate that quorum can be reached with sufficient FOR votes
-    function testQuorumCanBeReached() public {}
+    function testQuorumCanBeReached() public {
+        vm.prank(userWithQuorumTribe);
+        nopeDAO.castVote(proposalId, 1);
+        assertEq(uint8(nopeDAO.state(proposalId)), 4);
+    }
 }
