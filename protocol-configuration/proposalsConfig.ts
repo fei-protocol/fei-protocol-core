79e98febf0747e617ea647b98247a24212344291
Erwan Beauvois <erwan@allmyt.eu>
2022-08-12 11:56:47 +0200

    move tip_119 to old folder (already executed)

--- protocol-configuration/proposalsConfig.ts
+++ protocol-configuration/proposalsConfig.ts
@@ -1,19 +1,9 @@
 import { ProposalCategory, TemplatedProposalsConfigMap } from '@custom-types/types';
 
 import fip_x from '@proposals/description/fip_x';
-import tip_119 from '@proposals/description/tip_119';
 import tip_120 from '@proposals/description/tip_120';
 
 export const ProposalsConfig: TemplatedProposalsConfigMap = {
-  tip_119: {
-    deploy: false, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
-    totalValue: 0, // amount of ETH to send to DAO execution
-    proposal: tip_119, // full proposal file, imported from '@proposals/description/fip_xx.ts'
-    proposalId: '',
-    affectedContractSignoff: [],
-    deprecatedContractSignoff: [],
-    category: ProposalCategory.TC
-  },
   tip_120: {
     deploy: true, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
     totalValue: 0, // amount of ETH to send to DAO execution
