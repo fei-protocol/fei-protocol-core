a8c8b74ab39564cc80df20e87ac4d82470fe29ae
Erwan Beauvois <erwan@allmyt.eu>
2022-08-12 11:47:15 +0200

    Update proposals config to add tip_120

--- protocol-configuration/proposalsConfig.ts
+++ protocol-configuration/proposalsConfig.ts
@@ -2,12 +2,22 @@ import { ProposalCategory, TemplatedProposalsConfigMap } from '@custom-types/typ
 
 import fip_x from '@proposals/description/fip_x';
 import tip_119 from '@proposals/description/tip_119';
+import tip_120 from '@proposals/description/tip_120';
 
 export const ProposalsConfig: TemplatedProposalsConfigMap = {
   tip_119: {
-    deploy: true, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
+    deploy: false, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
     totalValue: 0, // amount of ETH to send to DAO execution
     proposal: tip_119, // full proposal file, imported from '@proposals/description/fip_xx.ts'
+    proposalId: '',
+    affectedContractSignoff: [],
+    deprecatedContractSignoff: [],
+    category: ProposalCategory.TC
+  },
+  tip_120: {
+    deploy: true, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
+    totalValue: 0, // amount of ETH to send to DAO execution
+    proposal: tip_120, // full proposal file, imported from '@proposals/description/fip_xx.ts'
     proposalId: '',
     affectedContractSignoff: [],
     deprecatedContractSignoff: [],
