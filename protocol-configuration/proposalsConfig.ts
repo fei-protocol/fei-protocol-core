2ec43cd52c6559177b57183cd4c0865b25569c31
Caleb Ditchfield <ditchfieldcaleb@gmail.com>
2022-08-25 16:06:32 -0700

    Update proposalsConfig with actual proposal ID now that it is live

--- protocol-configuration/proposalsConfig.ts
+++ protocol-configuration/proposalsConfig.ts
@@ -8,7 +8,7 @@ export const ProposalsConfig: TemplatedProposalsConfigMap = {
     deploy: false, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
     totalValue: 0, // amount of ETH to send to DAO execution
     proposal: tip_121a, // full proposal file, imported from '@proposals/description/fip_xx.ts'
-    proposalId: '',
+    proposalId: '39342228769990539577850858055862613427527095004175636100227557913556282521482',
     affectedContractSignoff: [],
     deprecatedContractSignoff: [],
     category: ProposalCategory.DAO
