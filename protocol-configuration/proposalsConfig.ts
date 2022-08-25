12eb2e47604824f36d58baefad0d3b0840bb4a41
thomas-waite <thomas.waite4@gmail.com>
2022-08-25 10:07:58 +0200

    feat: initial commit

--- protocol-configuration/proposalsConfig.ts
+++ protocol-configuration/proposalsConfig.ts
@@ -2,16 +2,16 @@ import { ProposalCategory, TemplatedProposalsConfigMap } from '@custom-types/typ
 
 import fip_x from '@proposals/description/fip_x';
 
-import phase_1 from '@proposals/description/phase_1';
+import tc_consolidate from '@proposals/description/tc_consolidate';
 
 export const ProposalsConfig: TemplatedProposalsConfigMap = {
-  phase_1: {
-    deploy: false, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
+  tc_consolidate: {
+    deploy: true, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
     totalValue: 0, // amount of ETH to send to DAO execution
-    proposal: phase_1, // full proposal file, imported from '@proposals/description/fip_xx.ts'
+    proposal: tc_consolidate, // full proposal file, imported from '@proposals/description/fip_xx.ts'
     proposalId: '',
     affectedContractSignoff: [],
     deprecatedContractSignoff: [],
-    category: ProposalCategory.DAO
+    category: ProposalCategory.TC
   }
 };
