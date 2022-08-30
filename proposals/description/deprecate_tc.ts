0464f36e3cddf9ab501a65c883ccff6aaf7ae9d0
thomas-waite <thomas.waite4@gmail.com>
2022-08-30 16:49:34 -0700

    feat: setup deprecate tc proposal

---
+++ proposals/description/deprecate_tc.ts
@@ -0,0 +1,20 @@
+import { TemplatedProposalDescription } from '@custom-types/types';
+
+const deprecate_tc: TemplatedProposalDescription = {
+  title: 'Deprecate Optimistic Governance and Tribal Council',
+  commands: [
+    {
+      target: '',
+      values: '',
+      method: '',
+      arguments: (addresses) => [],
+      description: ''
+    }
+  ],
+  description: `
+  [TITLE] /n/n
+  [BODY OF PROPOSAL] \n\n
+  `
+};
+
+export default deprecate_tc;
--- protocol-configuration/proposalsConfig.ts
+++ protocol-configuration/proposalsConfig.ts
@@ -2,12 +2,22 @@ import { ProposalCategory, TemplatedProposalsConfigMap } from '@custom-types/typ
 
 import fip_x from '@proposals/description/fip_x';
 import tc_consolidate from '@proposals/description/tc_consolidate';
+import deprecate_tc from '@proposals/description/deprecate_tc';
 
 export const ProposalsConfig: TemplatedProposalsConfigMap = {
   tc_consolidate: {
     deploy: true, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
     totalValue: 0, // amount of ETH to send to DAO execution
     proposal: tc_consolidate, // full proposal file, imported from '@proposals/description/fip_xx.ts'
+    proposalId: '',
+    affectedContractSignoff: [],
+    deprecatedContractSignoff: [],
+    category: ProposalCategory.TC
+  },
+  deprecate_tc: {
+    deploy: true, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
+    totalValue: 0, // amount of ETH to send to DAO execution
+    proposal: deprecate_tc, // full proposal file, imported from '@proposals/description/fip_xx.ts'
     proposalId: '',
     affectedContractSignoff: [],
     deprecatedContractSignoff: [],
