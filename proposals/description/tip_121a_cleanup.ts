d2405fd21e1cfced56f8c4480ef5123b78e9d48f
thomas-waite <thomas.waite4@gmail.com>
2022-09-08 16:38:18 +0100

    feat: initial commit and proposal setup

---
+++ proposals/description/tip_121a_cleanup.ts
@@ -0,0 +1,20 @@
+import { TemplatedProposalDescription } from '@custom-types/types';
+
+const fip_x: TemplatedProposalDescription = {
+  title: 'FIP-X: Title',
+  commands: [
+    {
+      target: '',
+      values: '',
+      method: '',
+      arguments: (addresses) => [],
+      description: ''
+    }
+  ],
+  description: `
+  [TITLE] /n/n
+  [BODY OF PROPOSAL] \n\n
+  `
+};
+
+export default fip_x;
