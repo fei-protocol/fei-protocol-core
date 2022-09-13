4029c418473b5bdd71b5a6144b48830ce3914d5e
thomas-waite <thomas.waite4@gmail.com>
2022-09-13 23:52:17 +0100

    fix: fix merge issues

--- protocol-configuration/proposalsConfig.ts
+++ protocol-configuration/proposalsConfig.ts
@@ -7,6 +7,15 @@ import tip_121c from '@proposals/description/tip_121c';
 import tip_121a_pt2 from '@proposals/description/tip_121a_pt2';
 
 export const ProposalsConfig: TemplatedProposalsConfigMap = {
+  tip_121a_pt2: {
+    deploy: true, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
+    totalValue: 0, // amount of ETH to send to DAO execution
+    proposal: tip_121a_pt2, // full proposal file, imported from '@proposals/description/fip_xx.ts'
+    proposalId: '',
+    affectedContractSignoff: [],
+    deprecatedContractSignoff: [],
+    category: ProposalCategory.TC
+  },
   tip_121b: {
     deploy: true,
     totalValue: 0,
@@ -23,16 +32,7 @@ export const ProposalsConfig: TemplatedProposalsConfigMap = {
     proposalId: '',
     affectedContractSignoff: [],
     deprecatedContractSignoff: [],
-    category: ProposalCategory.TC
-  },
-  tip_121a_pt2: {
-    deploy: true, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
-    totalValue: 0, // amount of ETH to send to DAO execution
-    proposal: tip_121a_pt2, // full proposal file, imported from '@proposals/description/fip_xx.ts'
-    proposalId: '',
-    affectedContractSignoff: [],
-    deprecatedContractSignoff: [],
-    category: ProposalCategory.TC
+    category: ProposalCategory.DAO
   }
 };
 
