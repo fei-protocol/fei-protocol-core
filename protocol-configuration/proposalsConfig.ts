850154938e2784ee8bfe29f8fde5085d8a6a41db
Joey Santoro <joseph.santoro@duke.edu>
2022-09-15 13:58:08 -0700

    cleanup

--- protocol-configuration/proposalsConfig.ts
+++ protocol-configuration/proposalsConfig.ts
@@ -8,15 +8,15 @@ import tip_121a_pt2 from '@proposals/description/tip_121a_pt2';
 import tip_121a_pt3 from '@proposals/description/tip_121a_pt3';
 
 export const ProposalsConfig: TemplatedProposalsConfigMap = {
-  // tip_121a_pt2: {
-  //   deploy: true, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
-  //   totalValue: 0, // amount of ETH to send to DAO execution
-  //   proposal: tip_121a_pt2, // full proposal file, imported from '@proposals/description/fip_xx.ts'
-  //   proposalId: '',
-  //   affectedContractSignoff: [],
-  //   deprecatedContractSignoff: [],
-  //   category: ProposalCategory.TC
-  // },
+  tip_121a_pt2: {
+    deploy: true, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
+    totalValue: 0, // amount of ETH to send to DAO execution
+    proposal: tip_121a_pt2, // full proposal file, imported from '@proposals/description/fip_xx.ts'
+    proposalId: '',
+    affectedContractSignoff: [],
+    deprecatedContractSignoff: [],
+    category: ProposalCategory.TC
+  },
   tip_121a_pt3: {
     deploy: false, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
     totalValue: 0, // amount of ETH to send to DAO execution
@@ -34,16 +34,16 @@ export const ProposalsConfig: TemplatedProposalsConfigMap = {
     affectedContractSignoff: [],
     deprecatedContractSignoff: [],
     category: ProposalCategory.DAO
+  },
+  tip_121c: {
+    deploy: true, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
+    totalValue: 0, // amount of ETH to send to DAO execution
+    proposal: tip_121c, // full proposal file, imported from '@proposals/description/fip_xx.ts'
+    proposalId: '',
+    affectedContractSignoff: [],
+    deprecatedContractSignoff: [],
+    category: ProposalCategory.DAO
   }
-  // tip_121c: {
-  //   deploy: true, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
-  //   totalValue: 0, // amount of ETH to send to DAO execution
-  //   proposal: tip_121c, // full proposal file, imported from '@proposals/description/fip_xx.ts'
-  //   proposalId: '',
-  //   affectedContractSignoff: [],
-  //   deprecatedContractSignoff: [],
-  //   category: ProposalCategory.DAO
-  // }
 };
 
 export default ProposalsConfig;
--- test/integration/tests/backstop.ts
+++ test/integration/tests/backstop.ts
@@ -9,7 +9,7 @@ import { ethers } from 'hardhat';
 
 const toBN = ethers.BigNumber.from;
 
-describe.only('e2e-backstop', function () {
+describe('e2e-backstop', function () {
   let contracts: NamedContracts;
   let contractAddresses: NamedAddresses;
   let deployAddress: string;
