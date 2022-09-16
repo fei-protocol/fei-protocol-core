f262fa1037bff13d6a79dd7c90ed71f2b0335a88
thomas-waite <thomas.waite4@gmail.com>
2022-09-16 14:12:38 +0100

    feat: initial commit, proposal setup

---
+++ proposals/description/tip_121c_pt2.ts
@@ -0,0 +1,19 @@
+import { TemplatedProposalDescription } from '@custom-types/types';
+
+const tip_121c_pt2: TemplatedProposalDescription = {
+  title: 'TIP_121c (cont.): Tribe Redemption',
+  commands: [
+    {
+      target: '',
+      values: '',
+      method: '',
+      arguments: (addresses) => [],
+      description: ''
+    }
+  ],
+  description: `
+  TIP_121c (cont.): Tribe Redemption
+  `
+};
+
+export default tip_121c_pt2;
--- protocol-configuration/proposalsConfig.ts
+++ protocol-configuration/proposalsConfig.ts
@@ -7,6 +7,7 @@ import tip_121c from '@proposals/description/tip_121c';
 import tip_121a_pt2 from '@proposals/description/tip_121a_pt2';
 import tip_121a_pt3 from '@proposals/description/tip_121a_pt3';
 import tip_indexotc from '@proposals/description/tip_indexotc';
+import tip_121c_pt2 from '@proposals/description/tip_121c_pt2';
 
 export const ProposalsConfig: TemplatedProposalsConfigMap = {
   tip_121a_pt2: {
@@ -49,6 +50,15 @@ export const ProposalsConfig: TemplatedProposalsConfigMap = {
     deploy: false, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
     totalValue: 0, // amount of ETH to send to DAO execution
     proposal: tip_indexotc, // full proposal file, imported from '@proposals/description/fip_xx.ts'
+    proposalId: '',
+    affectedContractSignoff: [],
+    deprecatedContractSignoff: [],
+    category: ProposalCategory.DAO
+  },
+  tip_121c_pt2: {
+    deploy: true, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
+    totalValue: 0, // amount of ETH to send to DAO execution
+    proposal: tip_121c_pt2, // full proposal file, imported from '@proposals/description/fip_xx.ts'
     proposalId: '',
     affectedContractSignoff: [],
     deprecatedContractSignoff: [],
