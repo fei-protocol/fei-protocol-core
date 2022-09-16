8ce0f5200a1dc141386ec5015ab8b6fc1632f4b1
thomas-waite <thomas.waite4@gmail.com>
2022-09-16 16:40:41 +0100

    feat: first proposal PCV transfer steps

--- proposals/dao/tip_121c_pt2.ts
+++ proposals/dao/tip_121c_pt2.ts
@@ -14,14 +14,40 @@ Tribe Redemption
 
 */
 
-const fipNumber = '9001'; // Change me!
+// Configuration
+
+// Circulating amount of TRIBE, which is redeemable for underlying PCV assets
+// TODO: Update with final circulating TRIBE figure
+const REDEEM_BASE = ethers.constants.WeiPerEther.mul(500_000_000);
+
+// Lido deposit balance, being withdrawn and sent to Tribe Redeemer
+// TODO: Check
+const STETH_DEPOSIT_BALANCE = '50296523674661485703301';
+
+// Minimum amount of DAI redeemable for Tribe (total DAI held - amount on FeiPSM)
+// TODO: Update with final numbers
+const MIN_DAI_REEMABLE = ethers.constants.WeiPerEther.mul(60_000_000);
+
+const fipNumber = 'tip_121c_pt2';
 
 // Do any deployments
 // This should exclusively include new contract deployments
 const deploy: DeployUpgradeFunc = async (deployAddress: string, addresses: NamedAddresses, logging: boolean) => {
-  console.log(`No deploy actions for fip${fipNumber}`);
+  const tribeRedeemerFactory = await ethers.getContractFactory('TribeRedeemer');
+
+  // Asset which can be redeemed for underlying PCV
+  const redeemedToken = addresses.tribe;
+
+  // Assets which the redeemedToken is redeemable for
+  // TODO: Verify these tokensReceived
+  const tokensReceived = [addresses.steth, addresses.lqty, addresses.fox];
+
+  const tribeRedeemer = await tribeRedeemerFactory.deploy(redeemedToken, tokensReceived, REDEEM_BASE);
+  await tribeRedeemer.deployTransaction.wait();
+  console.log('Tribe redeemer deployed to: ', tribeRedeemer.address);
+
   return {
-    // put returned contract objects here
+    tribeRedeemer
   };
 };
 
@@ -41,7 +67,24 @@ const teardown: TeardownUpgradeFunc = async (addresses, oldContracts, contracts,
 // Run any validations required on the fip using mocha or console logging
 // IE check balances, check state of contracts, etc.
 const validate: ValidateUpgradeFunc = async (addresses, oldContracts, contracts, logging) => {
-  console.log(`No actions to complete in validate for fip${fipNumber}`);
+  // 0. Verify Tribe Redeemer contract deploy params
+  expect(await contracts.tribeRedeemer.redeemBase()).to.equal(REDEEM_BASE);
+  expect(await contracts.tribeRedeemer.redeemedToken()).to.equal(addresses.tribe);
+
+  const expectedTokensReceived = [addresses.steth, addresses.lqty, addresses.fox];
+  const actualTokensReceived = await contracts.tribeRedeemer.tokensReceivedOnRedeem();
+  expect(actualTokensReceived.length).to.equal(expectedTokensReceived.length);
+  expect(actualTokensReceived).to.contain(actualTokensReceived[0]);
+  expect(actualTokensReceived).to.contain(actualTokensReceived[1]);
+  expect(actualTokensReceived).to.contain(actualTokensReceived[2]);
+
+  // 1. Verify Tribe Redeemer has all PCV assets deposited on it
+  expect(await contracts.steth.balanceOf(addresses.tribeRedeemer)).to.equal(STETH_DEPOSIT_BALANCE);
+  expect(await contracts.dai.balanceOf(addresses.tribeRedeemer)).to.be.bignumber.greaterThan(MIN_DAI_REEMABLE);
+
+  // 2. Verify previewRedeem() gives a reasonable value
+
+  // 3. Verify redeem() looks reasonable
 };
 
 export { deploy, setup, teardown, validate };
--- proposals/description/tip_121c_pt2.ts
+++ proposals/description/tip_121c_pt2.ts
@@ -1,15 +1,40 @@
 import { TemplatedProposalDescription } from '@custom-types/types';
 
+const MAX_BASIS_POINTS = '10000'; // 100% in basis points
+
 const tip_121c_pt2: TemplatedProposalDescription = {
   title: 'TIP_121c (cont.): Tribe Redemption',
   commands: [
+    // 1. Transfer PCV assets to TribeRedeemer for redemption
+    // stETH
     {
-      target: '',
-      values: '',
-      method: '',
-      arguments: (addresses) => [],
-      description: ''
+      target: 'ratioPCVControllerV2',
+      values: '0',
+      method: 'withdrawRatioERC20(address,address,address,uint256)',
+      arguments: (addresses) => [
+        addresses.ethLidoPCVDeposit,
+        addresses.steth,
+        addresses.tribeRedeemer,
+        MAX_BASIS_POINTS
+      ],
+      description: 'Withdraw all 50.3k stETH from Lido deposit to the Tribe Redeemer'
+    },
+    // DAI
+    {
+      target: 'ratioPCVControllerV2',
+      values: '0',
+      method: 'withdrawRatio(address,address,uint256)',
+      arguments: (addresses) => [addresses.daiHoldingPCVDeposit, addresses.tribeRedeemer, MAX_BASIS_POINTS],
+      description: 'Withdraw all ~68M DAI from holding deposit to the Tribe Redeemer'
     }
+    // FOX
+    // {
+    //   target: 'ratioPCVControllerV2',
+    //   values: '0',
+    //   method: 'withdraw(address,uint256)',
+    //   arguments: (addresses) => [],
+    //   description: 'Withdraw all ~ FOX to the Tribe Redeemer'
+    // }
   ],
   description: `
   TIP_121c (cont.): Tribe Redemption
