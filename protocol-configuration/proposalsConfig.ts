4f253fe60438434d3afee62970dc1aed8b34276a
thomas-waite <thomas.waite4@gmail.com>
2022-09-20 21:36:07 +0100

    feat: account for latest proposal execute

--- protocol-configuration/proposalsConfig.ts
+++ protocol-configuration/proposalsConfig.ts
@@ -1,20 +1,10 @@
 import { ProposalCategory, TemplatedProposalsConfigMap } from '@custom-types/types';
 
 import fip_x from '@proposals/description/fip_x';
-import tip_121b from '@proposals/description/tip_121b';
 import tip_121c from '@proposals/description/tip_121c';
 import tip_auraotc from '@proposals/description/tip_auraotc';
 
 export const ProposalsConfig: TemplatedProposalsConfigMap = {
-  tip_121b: {
-    deploy: false,
-    totalValue: 0,
-    proposal: tip_121b,
-    proposalId: '18409504155893955395764219200342193055990239653098975117323864343432865890837',
-    affectedContractSignoff: [],
-    deprecatedContractSignoff: [],
-    category: ProposalCategory.DAO
-  },
   tip_121c: {
     deploy: true, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
     totalValue: 0, // amount of ETH to send to DAO execution
