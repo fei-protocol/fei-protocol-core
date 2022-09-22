687583c61f899161724a26521e0417965d1834da
Caleb <ditchfieldcaleb@gmail.com>
2022-09-22 01:33:37 -0700

    Remove aura-otc from proposalsConfig

--- protocol-configuration/proposalsConfig.ts
+++ protocol-configuration/proposalsConfig.ts
@@ -2,18 +2,8 @@ import { ProposalCategory, TemplatedProposalsConfigMap } from '@custom-types/typ
 
 import fip_x from '@proposals/description/fip_x';
 import tip_121c from '@proposals/description/tip_121c';
-import tip_auraotc from '@proposals/description/tip_auraotc';
 
 export const ProposalsConfig: TemplatedProposalsConfigMap = {
-  tip_auraotc: {
-    deploy: false, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
-    totalValue: 0, // amount of ETH to send to DAO execution
-    proposal: tip_auraotc, // full proposal file, imported from '@proposals/description/fip_xx.ts'
-    proposalId: '40494215678183079926634175176986219126439592295510157269103853910881682985593',
-    affectedContractSignoff: [],
-    deprecatedContractSignoff: [],
-    category: ProposalCategory.DAO
-  },
   tip_121c: {
     deploy: true, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
     totalValue: 0, // amount of ETH to send to DAO execution
