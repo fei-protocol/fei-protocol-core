e01b30420ff497f0e7c8813e3121184e7aef54d8
thomas-waite <thomas.waite4@gmail.com>
2022-10-12 11:15:46 +0100

    feat: rari proposal verification

---
+++ proposals/dao/verify_rari_proposal.ts
@@ -0,0 +1,69 @@
+import hre, { ethers, artifacts } from 'hardhat';
+import { expect } from 'chai';
+import {
+  DeployUpgradeFunc,
+  NamedAddresses,
+  SetupUpgradeFunc,
+  TeardownUpgradeFunc,
+  ValidateUpgradeFunc
+} from '@custom-types/types';
+import { BigNumber } from 'ethers';
+
+const toBN = ethers.BigNumber.from;
+
+/*
+
+Verify Rari Proposal 
+
+*/
+
+let initialFuseMultisigUSDC: BigNumber;
+let initialFuseMultisigDAI: BigNumber;
+
+// Expected Fuse multisig asset gains
+const EXPECTED_USDC_GAIN = toBN('336235000000');
+const EXPECTED_DAI_GAIN = toBN('94218000000000000000000');
+
+const DAO_TIMELOCK_BURNER_ADDRESS = '0x6F6580285a63f1e886548458f427f8695BA1a563';
+
+const fipNumber = 'verify_rari_proposal';
+
+// Do any deployments
+// This should exclusively include new contract deployments
+const deploy: DeployUpgradeFunc = async (deployAddress: string, addresses: NamedAddresses, logging: boolean) => {
+  console.log(`No deploy actions for fip${fipNumber}`);
+  return {
+    // put returned contract objects here
+  };
+};
+
+// Do any setup necessary for running the test.
+// This could include setting up Hardhat to impersonate accounts,
+// ensuring contracts have a specific state, etc.
+const setup: SetupUpgradeFunc = async (addresses, oldContracts, contracts, logging) => {
+  initialFuseMultisigUSDC = await contracts.usdc.balanceOf(addresses.fuseMultisig);
+  initialFuseMultisigDAI = await contracts.dai.balanceOf(addresses.fuseMultisig);
+};
+
+// Tears down any changes made in setup() that need to be
+// cleaned up before doing any validation checks.
+const teardown: TeardownUpgradeFunc = async (addresses, oldContracts, contracts, logging) => {
+  console.log(`No actions to complete in teardown for fip${fipNumber}`);
+};
+
+// Run any validations required on the fip using mocha or console logging
+// IE check balances, check state of contracts, etc.
+const validate: ValidateUpgradeFunc = async (addresses, oldContracts, contracts, logging) => {
+  // 1. Verify Fuse multisig USDC gain
+  const usdcGain = (await contracts.usdc.balanceOf(addresses.fuseMultisig)).sub(initialFuseMultisigUSDC);
+  expect(usdcGain).to.be.bignumber.greaterThan(EXPECTED_USDC_GAIN);
+
+  // 2. Verify Fuse multisig DAI gain
+  const daiGain = (await contracts.dai.balanceOf(addresses.fuseMultisig)).sub(initialFuseMultisigDAI);
+  expect(daiGain).to.be.bignumber.greaterThan(EXPECTED_DAI_GAIN);
+
+  // 3. Verify Rari timelock admin set to DAOTimelockBurne
+  expect(await contracts.rariTimelock.admin()).to.equal(DAO_TIMELOCK_BURNER_ADDRESS);
+};
+
+export { deploy, setup, teardown, validate };
--- protocol-configuration/proposalsConfig.ts
+++ protocol-configuration/proposalsConfig.ts
@@ -1,17 +1,17 @@
 import { ProposalCategory, TemplatedProposalsConfigMap } from '@custom-types/types';
 
-import fip_x from '@proposals/description/fip_x';
+import verify_rari_proposal from '@proposals/description/verify_rari_proposal';
 
 export const ProposalsConfig: TemplatedProposalsConfigMap = {
-  // fip_x: {
-  //   deploy: false, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
-  //   totalValue: 0, // amount of ETH to send to DAO execution
-  //   proposal: fip_x, // full proposal file, imported from '@proposals/description/fip_xx.ts'
-  //   proposalId: '',
-  //   affectedContractSignoff: [],
-  //   deprecatedContractSignoff: [],
-  //   category: ProposalCategory.DAO
-  // }
+  verify_rari_proposal: {
+    deploy: false, // deploy flag for whether to run deploy action during e2e tests or use mainnet state
+    totalValue: 0, // amount of ETH to send to DAO execution
+    proposal: verify_rari_proposal, // full proposal file, imported from '@proposals/description/fip_xx.ts'
+    proposalId: '',
+    affectedContractSignoff: [],
+    deprecatedContractSignoff: [],
+    category: ProposalCategory.DAO
+  }
 };
 
 export default ProposalsConfig;
